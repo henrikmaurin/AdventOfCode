f660c8a9c01ee14b2112e33de741c1818dc10cf6
Henrik Maurin <henrik@henrikmaurin.se>
2021-12-15 22:55:59 +0100

    Day 12

---
+++ AdventOfCode2021/Day12.cs
@@ -0,0 +1,66 @@
+﻿using AdventOfCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2021
+{
+    public class Day12 : DayBase
+    {
+        public string[] instructions;
+
+        public int Problem1()
+        {
+            instructions = input.GetDataCached(2021, 12).SplitOnNewline().Where(s => !string.IsNullOrEmpty(s)).ToArray();
+           
+
+            return Visit("start",new List<string>());
+        }
+
+
+        public Int64 Problem2()
+        {
+         instructions = input.GetDataCached(2021, 12).SplitOnNewline().Where(s => !string.IsNullOrEmpty(s)).ToArray();
+
+
+
+            return Visit("start", new List<string>(),false);
+        }
+        public int Visit(string cave, List<string> visited, bool visitedTwice = true)
+        {
+            if (cave.ToLower() == cave)
+            {
+                if (visited.Contains(cave))
+                {
+                    if (visitedTwice || cave == "start")
+                    return 0;
+                    visitedTwice = true;
+                }
+            }
+
+           /* if (cave == "start"&& !first)
+                return 0;*/
+
+            if (cave == "end")
+                return 1;
+
+
+            List<string> localVisited = new List<string>();
+
+            localVisited.AddRange(visited);
+            localVisited.Add(cave);
+            int visits = 0;
+
+            foreach (string visitInstruction in instructions.Where(i => i.Contains(cave)))
+            {
+
+                visits += Visit(visitInstruction.Replace(cave, "").Replace("-", "").Trim(), localVisited,visitedTwice);
+            }
+
+            return visits;
+        }
+
+    }
+}
---
+++ AdventOfCode2021Tests/Day12Tests.cs
@@ -0,0 +1,97 @@
+﻿using AdventOfCode2021;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2021Tests
+{
+    [TestClass]
+    public class Day12Tests
+    {
+        Day12 day;
+
+        [TestInitialize]
+        public void Init()
+        {
+            day = new Day12();
+        }
+
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            day.instructions = new string[] {
+            "start-A",
+            "start-b",
+            "A-c",
+            "A-b",
+            "b-d",
+            "A-end",
+            "b-end"};
+
+            Assert.AreEqual(10, day.Visit("start", new List<string>()));
+
+
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            day.instructions = new string[] {
+            "dc-end",
+            "HN-start",
+            "start-kj",
+            "dc-start",
+            "dc-HN",
+            "LN-dc",
+            "HN-end",
+            "kj-sa",
+            "kj-HN",
+            "kj-dc",};
+
+            Assert.AreEqual(19, day.Visit("start", new List<string>()));
+
+
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            day.instructions = new string[] {
+            "start-A",
+            "start-b",
+            "A-c",
+            "A-b",
+            "b-d",
+            "A-end",
+            "b-end"};
+
+            Assert.AreEqual(36, day.Visit("start", new List<string>(),false));
+
+
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            day.instructions = new string[] {
+            "dc-end",
+            "HN-start",
+            "start-kj",
+            "dc-start",
+            "dc-HN",
+            "LN-dc",
+            "HN-end",
+            "kj-sa",
+            "kj-HN",
+            "kj-dc",};
+
+            Assert.AreEqual(103, day.Visit("start", new List<string>(),false));
+
+
+        }
+    }
+}
