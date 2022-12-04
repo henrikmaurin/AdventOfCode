3212ddee4f7063f658a12d472a08b94052818d37
Henrik Maurin <henrik@henrikmaurin.se>
2022-12-04 06:23:58 +0100

    2022, Day 4

--- AdventOfCode2022/Day04.cs
+++ AdventOfCode2022/Day04.cs
@@ -6,28 +6,66 @@ namespace AdventOfCode2022
     {
         private const int day = 4;
         List<string> data;
-        public Day04(bool runtests = false) : base(Global.Year, day, runtests)
+        public Day04(string testdata = null) : base(Global.Year, day, testdata != null)
         {
-            if (runtests)
+            if (testdata != null)
+            {
+                data = testdata.SplitOnNewline();
                 return;
+            }
 
             data = input.GetDataCached().SplitOnNewline();
         }
         public void Run()
         {
             int result1 = Problem1();
-            Console.WriteLine($"P1: Result: {result1}");
+            Console.WriteLine($"P1: Fully contains the other: {result1}");
 
             int result2 = Problem2();
-            Console.WriteLine($"P2: Result: {result2}");
+            Console.WriteLine($"P2: Overlaps eachother: {result2}");
         }
         public int Problem1()
         {
-            return 0;
+            return data.Select(d => FullyContains(d)).Sum();
         }
         public int Problem2()
         {
+            return data.Select(d => Overlaps(d)).Sum();
+        }
+
+        public int FullyContains(string s)
+        {
+            int[] sections = s.Replace("-", ",").Split(',').ToInt();
+
+            if (sections[0] >= sections[2] && sections[1] <= sections[3])
+                return 1;
+
+            if (sections[2] >= sections[0] && sections[3] <= sections[1])
+                return 1;
+
             return 0;
         }
+
+        public int Overlaps(string s)
+        {
+            int[] sections = s.Replace("-", ",").Split(',').ToInt();
+
+            if (sections[0].IsBetween(sections[2], sections[3]))
+                return 1;
+
+            if (sections[1].IsBetween(sections[2], sections[3]))
+                return 1;
+
+            if (sections[2].IsBetween(sections[0], sections[1]))
+                return 1;
+
+            if (sections[3].IsBetween(sections[0], sections[1]))
+                return 1;
+
+
+            return 0;
+        }
+
+
     }
 }
--- AdventOfCode2022Tests/UnitTestDay04.cs
+++ AdventOfCode2022Tests/UnitTestDay04.cs
@@ -1,4 +1,5 @@
 using AdventOfCode2022;
+using Common;
 
 namespace AdventOfCode2022Tests
 {
@@ -6,10 +7,21 @@ namespace AdventOfCode2022Tests
 	public class UnitTestDay04
 	{
 		private Day04 day;
+		private string data;
+		private List<string> testdata;
 		[TestInitialize]
 		public void Init()
 		{
-			day = new Day04(true);
+			data = @"2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8";
+			testdata = data.SplitOnNewline();
+
+
+			day = new Day04(data);
 		}
 
 
@@ -17,16 +29,38 @@ namespace AdventOfCode2022Tests
 		[TestCategory("Example data")]
 		public void Part1()
 		{
+			Assert.AreEqual(0, day.FullyContains(testdata[0]));
+			Assert.AreEqual(0, day.FullyContains(testdata[1]));
+			Assert.AreEqual(0, day.FullyContains(testdata[2]));
+			Assert.AreEqual(1, day.FullyContains(testdata[3]));
+			Assert.AreEqual(1, day.FullyContains(testdata[4]));
+			Assert.AreEqual(0, day.FullyContains(testdata[5]));
+		}
 
-			Assert.AreEqual(1, 1);
+		[TestMethod("Day 4, Part 1")]
+		[TestCategory("Problem")]
+		public void Part1_Problem1()
+		{
+			Assert.AreEqual(2, day.Problem1());
 		}
 
 		[TestMethod("Day 4, Part 2")]
 		[TestCategory("Example data")]
 		public void Part2()
 		{
+			Assert.AreEqual(0, day.Overlaps(testdata[0]));
+			Assert.AreEqual(0, day.Overlaps(testdata[1]));
+			Assert.AreEqual(1, day.Overlaps(testdata[2]));
+			Assert.AreEqual(1, day.Overlaps(testdata[3]));
+			Assert.AreEqual(1, day.Overlaps(testdata[4]));
+			Assert.AreEqual(1, day.Overlaps(testdata[5]));
+		}
 
-			Assert.AreEqual(2, 2);
+		[TestMethod("Day 4, Part 2")]
+		[TestCategory("Problem")]
+		public void Part1_Problem2()
+		{
+			Assert.AreEqual(4, day.Problem2());
 		}
 	}
 }
