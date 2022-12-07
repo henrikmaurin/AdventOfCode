92cb33ac8dbe81de333d2790c59f4b70994d5110
Henrik Maurin <henrik@henrikmaurin.se>
2022-12-07 12:00:51 +0100

    2022 Day 7,not cleaned

--- AdventOfCode2022/Day07.cs
+++ AdventOfCode2022/Day07.cs
@@ -6,10 +6,13 @@ namespace AdventOfCode2022
     {
         private const int day = 7;
         List<string> data;
-        public Day07(bool runtests = false) : base(Global.Year, day, runtests)
+        public Day07(string testdata = null) : base(Global.Year, day, testdata != null)
         {
-            if (runtests)
-                return;
+            if (testdata != null)
+            {
+                data = testdata.SplitOnNewline(); return;
+            }
+
 
             data = input.GetDataCached().SplitOnNewline();
         }
@@ -23,11 +26,118 @@ namespace AdventOfCode2022
         }
         public int Problem1()
         {
-            return 0;
+            return GetSumOfDirectories(data, 100000);
         }
         public int Problem2()
         {
             return 0;
         }
+
+        public int GetSumOfDirectories(List<string> lines, int maxSize)
+        {
+            int dirSum = 0;
+            Directory root = new Directory { Name = "/" };
+            root.ParentDir = root;
+            Directory currentDir = root;
+            int currentline = 0;
+            while (currentline < lines.Count)
+            {
+                if (lines[currentline].StartsWith("$ cd"))
+                {
+                    dirSum = 0;
+                    if (lines[currentline].Split(' ').Last() == "/")
+                        currentDir = root;
+                    else if (lines[currentline].Split(' ').Last() == "..")
+                    {
+                        currentDir = currentDir.ParentDir;
+                    }
+                    else
+                    {
+                        string dirname = lines[currentline].Split(' ').Last();
+                        currentDir = currentDir.Directories.Where(d => d.Name == dirname).Single();
+                    }
+
+                    currentline++;
+                    continue;
+                }
+
+                if (lines[currentline] == "$ ls" && currentline < lines.Count - 1)
+                {
+                    dirSum = 0;
+                    currentline++;
+                    while (currentline < lines.Count - 1 && !lines[currentline].StartsWith("$"))
+                    {
+                        if (lines[currentline].StartsWith("dir"))
+                        {
+                            Directory newDir = new Directory();
+                            newDir.Name = lines[currentline].Split(" ").Last();
+                            newDir.ParentDir = currentDir;
+                            currentDir.Directories.Add(newDir);
+
+                        }
+                        else if (int.TryParse(lines[currentline].Split(' ').First(), out int n))
+                            currentDir.FileSizes += n;
+                        currentline++;
+                    }
+
+
+                    continue;
+                }
+                currentline++;
+
+            }
+
+            var dirs = root.Flatten();
+
+            int used = root.GetTotalSize();
+
+            int totalfree = (70000000 - used);
+
+            int needToFree = 30000000 - totalfree;
+
+            int smallest = dirs.Where(d => d.GetTotalSize() >= needToFree).OrderBy(d => d.GetTotalSize()).Select(d => d.GetTotalSize()).First();
+            return smallest;
+
+
+            int sum = 0;
+            foreach (var dir in dirs)
+            {
+                int s = dir.GetTotalSize();
+                if (s <= maxSize)
+                    sum += s;
+
+            }
+
+
+            return sum;
+        }
+    }
+
+    class Directory
+    {
+        public static int Acc;
+        public string Name { get; set; }
+        public int FileSizes { get; set; }
+        public List<Directory> Directories { get; set; } = new List<Directory>();
+        public Directory ParentDir = null;
+
+        public int GetTotalSize()
+        {
+            int totalsize = FileSizes + Directories.Select(d => d.GetTotalSize()).Sum();
+
+            return totalsize;
+        }
+
+        public List<Directory> Flatten()
+        {
+            List<Directory> dirs = new List<Directory>();
+            dirs.Add(this);
+            foreach (Directory directory in Directories)
+            {
+                dirs.AddRange(directory.Flatten());
+            }
+            return dirs;
+        }
+
     }
 }
--- AdventOfCode2022Tests/UnitTestDay07.cs
+++ AdventOfCode2022Tests/UnitTestDay07.cs
@@ -1,4 +1,5 @@
 using AdventOfCode2022;
+using Common;
 
 namespace AdventOfCode2022Tests
 {
@@ -6,10 +7,38 @@ namespace AdventOfCode2022Tests
 	public class UnitTestDay07
 	{
 		private Day07 day;
+		private string data;
+		private List<string> testdata;
 		[TestInitialize]
 		public void Init()
 		{
-			day = new Day07(true);
+			data = @"$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k";
+
+			testdata = data.SplitOnNewline();
+
+			day = new Day07(data);
 		}
 
 
@@ -18,7 +47,7 @@ namespace AdventOfCode2022Tests
 		public void Part1()
 		{
 
-			Assert.AreEqual(1, 1);
+			Assert.AreEqual(95437, day.GetSumOfDirectories(testdata, 100000));
 		}
 
 		[TestMethod("Day 7, Part 2")]
