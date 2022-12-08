34309fe8cfdf9dc134d40a0b4933c5b8461e36f7
Henrik Maurin <henrik.maurin@soprasteria.com>
2022-12-08 17:08:30 +0100

    2022, Day 8

--- AdventOfCode2022/Day08.cs
+++ AdventOfCode2022/Day08.cs
@@ -1,17 +1,25 @@
 ﻿using Common;
+using System.Drawing;
+using System.Text;
 
 namespace AdventOfCode2022
 {
     public class Day08 : DayBase, IDay
     {
         private const int day = 8;
-        List<string> data;
-        public Day08(bool runtests = false) : base(Global.Year, day, runtests)
+        private List<string> data;
+        private Map2D<Tree> Map;
+        public Day08(string testdata = null) : base(Global.Year, day, testdata != null)
         {
-            if (runtests)
+            if (testdata != null)
+            {
+                data = testdata.SplitOnNewline();
+                Parse(data.ToArray());
                 return;
+            }
 
             data = input.GetDataCached().SplitOnNewline();
+            Parse(data.ToArray());
         }
         public void Run()
         {
@@ -23,11 +31,135 @@ namespace AdventOfCode2022
         }
         public int Problem1()
         {
-            return 0;
+           
+            return Map.Map.Where(m => m.Visible == true).Count();
         }
         public int Problem2()
         {
-            return 0;
+            return Map.Map.Select(m => m.ScenicScore).Max(); ;
         }
+
+        public void Parse(string[] mapdata)
+        {
+            int sizeX = mapdata[0].Length;
+            int sizeY = mapdata.Length;
+            Map = new Map2D<Tree>();
+            Map.SafeOperations = true;
+            Map.Init(sizeX, sizeY);
+
+            for (int y = 0; y < sizeY; y++)
+            {
+                for (int x = 0; x < sizeX; x++)
+                {
+                    Map[x, y] = new Tree();
+                    Map[x, y].Height = mapdata[y][x].ToInt();
+                }
+            }
+
+            for (int y = 0; y < sizeY; y++)
+            {
+                for (int x = 0; x < sizeX; x++)
+                {
+                    Map[x, y].Visible = CanSeeBorder(x, y);
+                    Map[x, y].ScenicScore = ScenicScore(x, y);
+                }
+            }
+
+        }
+
+        public bool CanSeeBorder(int xPos, int yPos)
+        {
+            bool canSeeLeft = true;
+            bool canSeeRight = true;
+            bool canSeeUp = true;
+            bool canSeeDown = true;
+
+       
+
+            for (int x = 0; x < xPos; x++)
+                if (Map[x, yPos].Height >= Map[xPos, yPos].Height)
+                    canSeeLeft = false;
+
+            for (int x = xPos + 1; x < Map.SizeX; x++)
+                if (Map[x, yPos].Height >= Map[xPos, yPos].Height)
+                    canSeeRight = false;
+
+            for (int y = 0; y < yPos; y++)
+                if (Map[xPos, y].Height >= Map[xPos, yPos].Height)
+                    canSeeUp = false;
+
+            for (int y = yPos + 1; y < Map.SizeY; y++)
+                if (Map[xPos, y].Height >= Map[xPos, yPos].Height)
+                    canSeeDown = false;
+
+
+            return canSeeDown || canSeeLeft || canSeeRight || canSeeUp;
+        }
+
+        public int ScenicScore(int xPos, int yPos)
+        {
+            int canSeeLeft = 0;
+            int canSeeRight = 0;
+            int canSeeUp = 0;
+            int canSeeDown = 0;
+
+            if (xPos == 2 && yPos == 3)
+            { int a = 0; }
+
+            if (xPos == 2 && yPos == 4)
+            {
+                int a = 0;
+            }
+
+            for (int x = xPos-1; x >=0; x--)
+                if (Map[x, yPos].Height < Map[xPos, yPos].Height)
+                    canSeeLeft++;
+                else
+                {
+                    canSeeLeft++;
+                    break;
+                }
+
+
+
+            for (int x = xPos + 1; x < Map.SizeX; x++)
+                if (Map[x, yPos].Height < Map[xPos, yPos].Height)
+                    canSeeRight++;
+                else
+                {
+                    canSeeRight++;
+                    break;
+                }
+
+            for (int y = yPos - 1; y >= 0; y--)
+                if (Map[xPos, y].Height < Map[xPos, yPos].Height)
+                    canSeeUp++;
+                else
+                {
+                    canSeeUp++;
+                    break;
+                }
+
+            for (int y = yPos + 1; y < Map.SizeY; y++)
+                if (Map[xPos, y].Height < Map[xPos, yPos].Height)
+                    canSeeDown++;
+                else
+                {
+                    canSeeDown++;
+                    break;
+                }
+              
+
+
+            return canSeeDown * canSeeLeft * canSeeRight * canSeeUp;
+
+        }
+    }
+
+    class Tree
+    {
+        public bool Visible { get; set; }
+        public int Height { get; set; }
+        public int ScenicScore { get; set; }
     }
 }
--- AdventOfCode2022Tests/UnitTestDay08.cs
+++ AdventOfCode2022Tests/UnitTestDay08.cs
@@ -1,32 +1,65 @@
 using AdventOfCode2022;
+using Common;
 
 namespace AdventOfCode2022Tests
 {
-	[TestClass]
-	public class UnitTestDay08
-	{
-		private Day08 day;
-		[TestInitialize]
-		public void Init()
-		{
-			day = new Day08(true);
-		}
+    [TestClass]
+    public class UnitTestDay08
+    {
+        private Day08 day;
+        private string data;
+        private List<string> testdata;
+        [TestInitialize]
+        public void Init()
+        {
+            data = @"30373
+25512
+65332
+33549
+35390";
+            testdata = data.SplitOnNewline();
 
 
-		[TestMethod("Day 8, Part 1")]
-		[TestCategory("Example data")]
-		public void Part1()
-		{
+            day = new Day08(data);
+        }
 
-			Assert.AreEqual(1, 1);
-		}
 
-		[TestMethod("Day 8, Part 2")]
-		[TestCategory("Example data")]
-		public void Part2()
-		{
+        [TestMethod("Day 8, Part 1")]
+        [TestCategory("Example data")]
+        public void Part1_1()
+        {
+            Assert.IsTrue(day.CanSeeBorder(1, 1));
+            Assert.IsTrue(day.CanSeeBorder(2, 1));
+            Assert.IsFalse(day.CanSeeBorder(3, 1));
+            Assert.IsTrue(day.CanSeeBorder(1, 2));
+            Assert.IsFalse(day.CanSeeBorder(2, 2));
+            Assert.IsTrue(day.CanSeeBorder(3, 2));
+            Assert.IsTrue(day.CanSeeBorder(2, 3));
+            Assert.IsFalse(day.CanSeeBorder(1, 3));
+            Assert.IsFalse(day.CanSeeBorder(3, 3));
+        }
 
-			Assert.AreEqual(2, 2);
-		}
-	}
+        [TestMethod("Day 8, Part 1")]
+        [TestCategory("Problem 1")]
+        public void Part1()
+        {
+            Assert.AreEqual(21, day.Problem1());
+        }
+
+        [TestMethod("Day 8, Part 2")]
+        [TestCategory("Example data")]
+        public void Part2_1()
+        {
+            Assert.AreEqual(4, day.ScenicScore(2, 1));
+            Assert.AreEqual(8, day.ScenicScore(2, 3));
+        }
+
+        [TestMethod("Day 8, Part 2")]
+        [TestCategory("Problem 2")]
+        public void Part2()
+        {
+
+            Assert.AreEqual(8, day.Problem2());
+        }
+    }
 }
