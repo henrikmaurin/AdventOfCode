0effa7cd9e61b7e6b8442e2a061b2ae33a805277
Henrik Maurin <henrik.maurin@soprasteria.com>
2022-12-09 07:44:20 +0100

    2022 Day 9

--- AdventOfCode2022/Day09.cs
+++ AdventOfCode2022/Day09.cs
@@ -1,15 +1,22 @@
 ﻿using Common;
+using System.Globalization;
 
 namespace AdventOfCode2022
 {
     public class Day09 : DayBase, IDay
     {
         private const int day = 9;
-        List<string> data;
-        public Day09(bool runtests = false) : base(Global.Year, day, runtests)
+        private List<string> data;
+        private Dictionary<string, int> visits;
+        public List<Coord2D> Knots;
+        public Day09(string testdata = null) : base(Global.Year, day, testdata != null)
         {
-            if (runtests)
+
+            if (testdata != null)
+            {
+                data = testdata.SplitOnNewline();
                 return;
+            }
 
             data = input.GetDataCached().SplitOnNewline();
         }
@@ -23,11 +30,103 @@ namespace AdventOfCode2022
         }
         public int Problem1()
         {
-            return 0;
+            Parse(data.ToArray());
+
+            return Visited();
         }
         public int Problem2()
         {
-            return 0;
+            Parse(data.ToArray(), 10);
+            return Visited();
         }
+
+        public void Parse(string[] instructions, int knots = 2)
+        {
+            Knots = new List<Coord2D>();
+            for (int i = 0; i < knots; i++)
+            {
+                Knots.Add(new Coord2D());
+            }
+
+            visits = new Dictionary<string, int>();
+            visits.Add("0,0", 1);
+
+            foreach (string instruction in instructions)
+            {
+                Move(instruction);
+            }
+        }
+
+        public int Visited()
+        {
+            return visits.Count;
+        }
+
+        public void Move(string instruction)
+        {
+            string direction = instruction.Split(" ").First();
+            int positions = instruction.Split(" ").Last().ToInt();
+            Coord2D dir = new Coord2D();
+            Coord2D lastTail = new Coord2D();
+
+            switch (direction)
+            {
+                case "R":
+                    dir = Directions.GetDirection(Directions.Right);
+                    break;
+
+                case "L":
+                    dir = Directions.GetDirection(Directions.Left);
+                    break;
+
+                case "U":
+                    dir = Directions.GetDirection(Directions.Top);
+                    break;
+
+                case "D":
+                    dir = Directions.GetDirection(Directions.Bottom);
+                    break;
+            }
+
+            for (int count = 0; count < positions; count++)
+            {
+                Knots[0] = Knots[0] + dir;
+                {
+                    for (int knot = 1; knot < Knots.Count; knot++)
+                    {
+                        if (Math.Abs(Knots[knot - 1].X - Knots[knot].X) > 1 || Math.Abs(Knots[knot - 1].Y - Knots[knot].Y) > 1)
+                        {
+                            Coord2D move = new Coord2D();
+                            if (Knots[knot - 1].X - Knots[knot].X > 0)
+                            {
+                                move += Directions.GetDirection(Directions.Right);
+                            }
+                            if (Knots[knot - 1].X - Knots[knot].X < 0)
+                            {
+                                move += Directions.GetDirection(Directions.Left);
+                            }
+                            if (Knots[knot - 1].Y - Knots[knot].Y > 0)
+                            {
+                                move += Directions.GetDirection(Directions.Bottom);
+                            }
+                            if (Knots[knot - 1].Y - Knots[knot].Y < 0)
+                            {
+                                move += Directions.GetDirection(Directions.Top);
+                            }
+                            Knots[knot] += move;
+                        }
+
+                    }
+                }
+                if (!visits.ContainsKey($"{Knots.Last().X},{Knots.Last().Y}"))
+                {
+                    visits.Add($"{Knots.Last().X},{Knots.Last().Y}", 1);
+                }
+                else
+                {
+                    visits[$"{Knots.Last().X},{Knots.Last().Y}"]++;
+                }
+            }
+        }      
     }
 }
--- AdventOfCode2022Tests/UnitTestDay09.cs
+++ AdventOfCode2022Tests/UnitTestDay09.cs
@@ -1,4 +1,5 @@
 using AdventOfCode2022;
+using Common;
 
 namespace AdventOfCode2022Tests
 {
@@ -6,10 +7,24 @@ namespace AdventOfCode2022Tests
 	public class UnitTestDay09
 	{
 		private Day09 day;
-		[TestInitialize]
+        private string data;
+        private string[] testdata;
+        [TestInitialize]
 		public void Init()
 		{
-			day = new Day09(true);
+			data = @"R 4
+U 4
+L 3
+D 1
+R 4
+D 1
+L 5
+R 2
+";
+
+			testdata = data.SplitOnNewlineArray();
+
+			day = new Day09(data);
 		}
 
 
@@ -17,16 +32,34 @@ namespace AdventOfCode2022Tests
 		[TestCategory("Example data")]
 		public void Part1()
 		{
-
-			Assert.AreEqual(1, 1);
+			day.Parse(testdata);
+			Assert.AreEqual(13, day.Visited());
 		}
 
-		[TestMethod("Day 9, Part 2")]
-		[TestCategory("Example data")]
-		public void Part2()
-		{
+        [TestMethod("Day 9, Part 1")]
+        [TestCategory("Example data")]
+        public void Part2_1()
+        {
+            day.Parse(testdata,10);
+            Assert.AreEqual(1, day.Visited());
+        }
 
-			Assert.AreEqual(2, 2);
+        [TestMethod("Day 9, Part 2")]
+		[TestCategory("Example data")]
+		public void Part2_2()
+		{
+			data = @"R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20
+";
+			testdata= data.SplitOnNewlineArray();
+            day.Parse(testdata,10);
+            Assert.AreEqual(36, day.Visited());
 		}
 	}
 }
