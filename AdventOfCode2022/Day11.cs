a830a7e0daae0588cf61089dbac6d4034b4a4956
Henrik Maurin <henrik.maurin@soprasteria.com>
2022-12-11 13:14:09 +0100

    2022 Day 11

--- AdventOfCode2022/Day11.cs
+++ AdventOfCode2022/Day11.cs
@@ -1,33 +1,133 @@
 ﻿using Common;
+using System.Reflection.Emit;
+using static AdventOfCode2022.Day11;
 
 namespace AdventOfCode2022
 {
     public class Day11 : DayBase, IDay
     {
         private const int day = 11;
-        List<string> data;
-        public Day11(bool runtests = false) : base(Global.Year, day, runtests)
-        {
-            if (runtests)
-                return;
+        string[][] data;
+        private Monkey[] monkeys;
 
-            data = input.GetDataCached().SplitOnNewline();
+        public Day11(string testdata = null) : base(Global.Year, day, testdata != null)
+        {
+            if (testdata != null)
+            {
+                data = testdata.GroupByEmptyLine();
+                return;
+            }
+
+
+            data = input.GetDataCached().GroupByEmptyLine();
         }
         public void Run()
         {
-            int result1 = Problem1();
+            long result1 = Problem1();
             Console.WriteLine($"P1: Result: {result1}");
 
-            int result2 = Problem2();
+            long result2 = Problem2();
             Console.WriteLine($"P2: Result: {result2}");
         }
-        public int Problem1()
+        public long Problem1()
         {
-            return 0;
+            Parse(data);
+
+            return CalcMonkeyBusiness(20);
         }
-        public int Problem2()
+        public long Problem2()
         {
-            return 0;
+            Parse(data);
+            return CalcMonkeyBusiness(10000, false);
+        }
+
+        public long CalcMonkeyBusiness(int rounds, bool KeepCalm = true)
+        {
+            long wl = 0;
+            long divider = 1;
+            foreach (Monkey m in monkeys)
+                divider *= m.DivisibleBy;
+
+            for (int r = 0; r < rounds; r++)
+            {
+                if (r.In(20, 1000, 2000))
+                { int a = 0; }
+
+
+                for (int monkey = 0; monkey < monkeys.Length; monkey++)
+                {
+                    for (int itemNo = 0; itemNo < monkeys[monkey].Items.Count; itemNo++)
+                    {
+                        monkeys[monkey].Inspections++;
+                        long operand = monkeys[monkey].OperationNumber;
+                        if (operand == 0)
+                            operand = monkeys[monkey].Items[itemNo];
+
+                        switch (monkeys[monkey].Operation)
+                        {
+                            case "+":
+                                wl = monkeys[monkey].Items[itemNo] + operand;
+                                break;
+                            case "-":
+                                wl = monkeys[monkey].Items[itemNo] - operand;
+                                break;
+                            case "*":
+                                wl = monkeys[monkey].Items[itemNo] * operand;
+                                break;
+                            case "/":
+                                wl = monkeys[monkey].Items[itemNo] / operand;
+                                break;
+                        }
+
+                        if (KeepCalm)
+                            wl = wl / 3;
+
+                        wl %= divider;
+
+                        if (wl % monkeys[monkey].DivisibleBy == 0)
+                        {
+                            monkeys[monkeys[monkey].IfTrue].Items.Add(wl);
+                        }
+                        else
+                        {
+                            monkeys[monkeys[monkey].IfFalse].Items.Add(wl);
+                        }
+
+                    }
+                    monkeys[monkey].Items.Clear();
+                }
+            }
+
+            long[] top2 = monkeys.OrderByDescending(m => m.Inspections).Take(2).Select(m => m.Inspections).ToArray();
+
+            return top2[0] * top2[1];
+        }
+
+        public void Parse(string[][] monkeydata)
+        {
+            monkeys = new Monkey[monkeydata.Length];
+            for (int i = 0; i < monkeys.Length; i++)
+            {
+                monkeys[i] = new Monkey();
+                monkeys[i].Items = monkeydata[i][1].Replace("Starting items: ", "").Split(",").ToLong().ToList();
+                monkeys[i].Operation = monkeydata[i][2].Split(" ")[6];
+                monkeys[i].OperationNumber = monkeydata[i][2].Split(" ").Last().ToLong();
+                monkeys[i].DivisibleBy = monkeydata[i][3].Split(" ").Last().ToLong();
+                monkeys[i].IfTrue = monkeydata[i][4].Split(" ").Last().ToInt();
+                monkeys[i].IfFalse = monkeydata[i][5].Split(" ").Last().ToInt();
+            }
+        }
+
+
+        public class Monkey
+        {
+            public List<long> Items { get; set; }
+            public string Operation { get; set; }
+            public long OperationNumber { get; set; }
+            public long DivisibleBy { get; set; }
+            public int IfTrue { get; set; }
+            public int IfFalse { get; set; }
+            public long Inspections { get; set; } = 0;
         }
     }
 }
--- AdventOfCode2022Tests/UnitTestDay11.cs
+++ AdventOfCode2022Tests/UnitTestDay11.cs
@@ -1,4 +1,5 @@
 using AdventOfCode2022;
+using Common;
 
 namespace AdventOfCode2022Tests
 {
@@ -6,10 +7,43 @@ namespace AdventOfCode2022Tests
 	public class UnitTestDay11
 	{
 		private Day11 day;
+		private string data;
+		string[][] testdata;
 		[TestInitialize]
 		public void Init()
 		{
-			day = new Day11(true);
+			data = @"Monkey 0:
+  Starting items: 79, 98
+  Operation: new = old * 19
+  Test: divisible by 23
+    If true: throw to monkey 2
+    If false: throw to monkey 3
+
+Monkey 1:
+  Starting items: 54, 65, 75, 74
+  Operation: new = old + 6
+  Test: divisible by 19
+    If true: throw to monkey 2
+    If false: throw to monkey 0
+
+Monkey 2:
+  Starting items: 79, 60, 97
+  Operation: new = old * old
+  Test: divisible by 13
+    If true: throw to monkey 1
+    If false: throw to monkey 3
+
+Monkey 3:
+  Starting items: 74
+  Operation: new = old + 3
+  Test: divisible by 17
+    If true: throw to monkey 0
+    If false: throw to monkey 1
+";
+
+			testdata = data.GroupByEmptyLine();
+
+			day = new Day11(data);
 		}
 
 
@@ -17,16 +51,16 @@ namespace AdventOfCode2022Tests
 		[TestCategory("Example data")]
 		public void Part1()
 		{
-
-			Assert.AreEqual(1, 1);
+			day.Parse(testdata);
+			Assert.AreEqual(10605, day.CalcMonkeyBusiness(20));
 		}
 
 		[TestMethod("Day 11, Part 2")]
 		[TestCategory("Example data")]
 		public void Part2()
 		{
-
-			Assert.AreEqual(2, 2);
-		}
+            day.Parse(testdata);
+            Assert.AreEqual(2713310158, day.CalcMonkeyBusiness(10000,false));
+        }
 	}
 }
