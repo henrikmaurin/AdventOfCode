6360f7e8652cd9486f6b2ad1b9ee6e3e26cdf2dc
Henrik Maurin <henrik@henrikmaurin.se>
2022-12-13 16:33:20 +0100

    2022, Day 13-incomplete

--- AdventOfCode2022/Day13.cs
+++ AdventOfCode2022/Day13.cs
@@ -5,13 +5,16 @@ namespace AdventOfCode2022
     public class Day13 : DayBase, IDay
     {
         private const int day = 13;
-        List<string> data;
-        public Day13(bool runtests = false) : base(Global.Year, day, runtests)
+        string[][] data;
+        public Day13(string testdata = null) : base(Global.Year, day, testdata != null)
         {
-            if (runtests)
+            if (testdata != null)
+            {
+                data = testdata.GroupByEmptyLine();
                 return;
+            }
 
-            data = input.GetDataCached().SplitOnNewline();
+            data = input.GetDataCached().GroupByEmptyLine();
         }
         public void Run()
         {
@@ -23,11 +26,132 @@ namespace AdventOfCode2022
         }
         public int Problem1()
         {
-            return 0;
+            return CalcRightOrder();
         }
         public int Problem2()
         {
             return 0;
         }
+
+        public int CalcRightOrder()
+        {
+            int sum = 0;
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (Compare2(data[i]))
+                    sum += 1 + i;
+            }
+            return sum;
+
+        }
+
+
+        public bool Compare2(string[] lines)
+        {
+            if (lines[0].Length == 0)
+                return true;
+            if (lines[1].Length == 0)
+                return false;
+
+            if (lines[0].StartsWith("["))
+            {
+                int leftEnd = FindEnd(lines[0].Substring(1));
+
+                lines[0] = lines[0].Substring(1, leftEnd - 1);
+
+
+                if (!lines[1].StartsWith("["))
+                    lines[1] = $"[{lines[1]}]";
+
+                int rightEnd = FindEnd(lines[1].Substring(1));
+
+                lines[1] = lines[1].Substring(1, rightEnd - 1);
+
+                return Compare2(lines);
+            }
+
+            if (lines[1].StartsWith("["))
+            {
+                lines[0] = $"[{lines[0]}]";
+                return Compare2(lines);
+            }
+
+            string[] leftSplit = lines[0].Split(",");
+            string[] rightSplit = lines[1].Split(",");
+            if (rightSplit.Count() < leftSplit.Count())
+                return false;
+
+            for (int i = 0; i < leftSplit.Length; i++)
+            {
+                if (int.TryParse(leftSplit[i].Replace("]", ""), out int leftNumber))
+                {
+
+                    if (int.TryParse(rightSplit[i].Replace("]", ""), out int rightNumber))
+                    {
+                        if (leftNumber < rightNumber)
+                            return true;
+
+                        if (rightNumber < leftNumber)
+                            return false;
+                    }
+                    else
+                    {
+                        string left = string.Join(',', leftSplit[i..]);
+                        string right = string.Join(',', rightSplit[i..]);
+
+                        if (left == "")
+                            return true;
+
+
+                        string[] newCompare = new string[2];
+                        newCompare[0] = left.Substring(1);
+                        newCompare[1] = right.Substring(1);
+
+                        return Compare2(newCompare);
+                    }
+                }
+                else
+                {
+                    string left = string.Join(',', leftSplit[i..]);
+                    string right = string.Join(',', rightSplit[i..]);
+
+                    if (left == "")
+                        return true;
+
+
+                    string[] newCompare = new string[2];
+                    newCompare[0] = left.Substring(1);
+                    newCompare[1] = right.Substring(1);
+
+                    return Compare2(newCompare);
+                }
+
+            }
+
+
+
+
+
+            return true;
+        }
+
+
+        public int FindEnd(string toFindIn)
+        {
+            int depth = 1;
+            int i = 0;
+            while (depth != 0)
+            {
+                if (toFindIn[i] == '[')
+                    depth++;
+                if (toFindIn[i] == ']')
+                { depth--; }
+                i++;
+            }
+            return i;
+        }
+
     }
+
+
 }
--- AdventOfCode2022Tests/UnitTestDay13.cs
+++ AdventOfCode2022Tests/UnitTestDay13.cs
@@ -1,4 +1,5 @@
 using AdventOfCode2022;
+using Common;
 
 namespace AdventOfCode2022Tests
 {
@@ -6,19 +7,119 @@ namespace AdventOfCode2022Tests
 	public class UnitTestDay13
 	{
 		private Day13 day;
+		private string data;
+		private string[][] testdata;
 		[TestInitialize]
 		public void Init()
 		{
-			day = new Day13(true);
+			data = @"[1,1,3,1,1]
+[1,1,5,1,1]
+
+[[1],[2,3,4]]
+[[1],4]
+
+[9]
+[[8,7,6]]
+
+[[4,4],4,4]
+[[4,4],4,4,4]
+
+[7,7,7,7]
+[7,7,7]
+
+[]
+[3]
+
+[[[]]]
+[[]]
+
+[1,[2,[3,[4,[5,6,7]]]],8,9]
+[1,[2,[3,[4,[5,6,0]]]],8,9]
+
+[1,[2,[3,[4,[5,6,7]]]],8,9]
+[1,[2,[3,[4,[5,6,8]]]],8,9]";
+
+			testdata = data.GroupByEmptyLine();
+
+			day = new Day13(data);
 		}
 
+
+		[TestMethod("Day 13, Part 1")]
+		[TestCategory("Example data")]
+		public void Part1_1()
+		{
+
+			Assert.AreEqual(true, day.Compare2(testdata[0]));
+		}
+		[TestMethod("Day 13, Part 1")]
+		[TestCategory("Example data")]
+		public void Part1_2()
+		{
+
+			Assert.AreEqual(true, day.Compare2(testdata[1]));
+		}
+		[TestMethod("Day 13, Part 1")]
+		[TestCategory("Example data")]
+		public void Part1_3()
+		{
+
+			Assert.AreEqual(false, day.Compare2(testdata[2]));
+		}
+
+		[TestMethod("Day 13, Part 1")]
+		[TestCategory("Example data")]
+		public void Part1_4()
+		{
+
+			Assert.AreEqual(true, day.Compare2(testdata[3]));
+		}
+
+		[TestMethod("Day 13, Part 1")]
+		[TestCategory("Example data")]
+		public void Part1_5()
+		{
+
+			Assert.AreEqual(false, day.Compare2(testdata[4]));
+		}
+
+		[TestMethod("Day 13, Part 1")]
+		[TestCategory("Example data")]
+		public void Part1_6()
+		{
+
+			Assert.AreEqual(true, day.Compare2(testdata[5]));
+		}
+
+		[TestMethod("Day 13, Part 1")]
+		[TestCategory("Example data")]
+		public void Part1_7()
+		{
+
+			Assert.AreEqual(false, day.Compare2(testdata[6]));
+		}
+
+		[TestMethod("Day 13, Part 1")]
+		[TestCategory("Example data")]
+		public void Part1_8()
+		{
+
+			Assert.AreEqual(false, day.Compare2(testdata[7]));
+		}
+		[TestMethod("Day 13, Part 1")]
+		[TestCategory("Example data")]
+		public void Part1_9()
+		{
+
+			Assert.AreEqual(true, day.Compare2(testdata[8]));
+		}
 
 		[TestMethod("Day 13, Part 1")]
 		[TestCategory("Example data")]
 		public void Part1()
 		{
 
-			Assert.AreEqual(1, 1);
+			Assert.AreEqual(22, day.CalcRightOrder());
 		}
 
 		[TestMethod("Day 13, Part 2")]
