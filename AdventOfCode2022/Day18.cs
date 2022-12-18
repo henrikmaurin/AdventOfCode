c144b4158175d7eee8c9c135f676bc7cb70c6e98
Henrik Maurin <henrik.maurin@soprasteria.com>
2022-12-18 18:23:51 +0100

    2022, Day 18 - Brute force part 2 with dumb flood fill

--- AdventOfCode2022/Day18.cs
+++ AdventOfCode2022/Day18.cs
@@ -6,10 +6,14 @@ namespace AdventOfCode2022
     {
         private const int day = 18;
         List<string> data;
-        public Day18(bool runtests = false) : base(Global.Year, day, runtests)
+        List<Droplet> droplets;
+        public Day18(string testdata = null) : base(Global.Year, day, testdata != null)
         {
-            if (runtests)
+            if (testdata != null)
+            {
+                data = testdata.SplitOnNewline();
                 return;
+            }
 
             data = input.GetDataCached().SplitOnNewline();
         }
@@ -23,11 +27,128 @@ namespace AdventOfCode2022
         }
         public int Problem1()
         {
-            return 0;
+            Parse(data);
+            FindNeigbors();
+            return CountExposed();
+
         }
         public int Problem2()
         {
-            return 0;
+            Parse(data);
+            AddVoid();
+            FindNeigbors();
+            return CountExposed();
+        }
+
+        public int CountExposed()
+        {
+            return droplets.Select(d => d.ExposedSides).Sum(); ;
+        }
+
+        public void Parse(List<string> dropletData)
+        {
+            droplets = new List<Droplet>();
+            foreach (string dropletLine in dropletData)
+            {
+                droplets.Add(new Droplet
+                {
+                    X = dropletLine.Split(",")[0].ToInt(),
+                    Y = dropletLine.Split(",")[1].ToInt(),
+                    Z = dropletLine.Split(",")[2].ToInt(),
+                    ExposedSides = 6,
+                    type = "lava",
+                });
+            }
+        }
+
+        public void FindNeigbors()
+        {
+            foreach (Droplet droplet in droplets)
+            {
+                if (droplets.Where(d => d.X == droplet.X - 1 && d.Y == droplet.Y && d.Z == droplet.Z).Any())
+                    droplet.ExposedSides--;
+
+                if (droplets.Where(d => d.X == droplet.X + 1 && d.Y == droplet.Y && d.Z == droplet.Z).Any())
+                    droplet.ExposedSides--;
+
+                if (droplets.Where(d => d.X == droplet.X && d.Y == droplet.Y - 1 && d.Z == droplet.Z).Any())
+                    droplet.ExposedSides--;
+
+                if (droplets.Where(d => d.X == droplet.X && d.Y == droplet.Y + 1 && d.Z == droplet.Z).Any())
+                    droplet.ExposedSides--;
+
+                if (droplets.Where(d => d.X == droplet.X && d.Y == droplet.Y && d.Z == droplet.Z - 1).Any())
+                    droplet.ExposedSides--;
+
+                if (droplets.Where(d => d.X == droplet.X && d.Y == droplet.Y && d.Z == droplet.Z + 1).Any())
+                    droplet.ExposedSides--;
+            }
+        }
+
+        public void AddVoid()
+        {
+            for (int x = droplets.Select(d => d.X).Min(); x <= droplets.Select(d => d.X).Max(); x++)
+            {
+                for (int y = droplets.Select(d => d.Y).Min(); y <= droplets.Select(d => d.Y).Max(); y++)
+                {
+                    for (int z = droplets.Select(d => d.Z).Min(); z <= droplets.Select(d => d.Z).Max(); z++)
+                    {
+                        if (droplets.Where(d => d.X == x && d.Y == y && d.Z == z).Any())
+                            continue;
+
+                        droplets.Add(new Droplet
+                        {
+                            X = x,
+                            Y = y,
+                            Z = z,
+                            ExposedSides = 6,
+                            type = "unknown"
+                        });
+
+                    }
+                }
+            }
+
+            int updated = 1;
+            while (updated > 0)
+            {
+                updated = 0;
+                foreach (Droplet droplet in droplets.Where(d => d.type == "unknown"))
+                {
+                    if (droplet.X <= droplets.Select(d => d.X).Min()
+                        || droplet.X >= droplets.Select(d => d.X).Max()
+                        || droplet.Y <= droplets.Select(d => d.Y).Min()
+                        || droplet.Y >= droplets.Select(d => d.Y).Max()
+                        || droplet.Z <= droplets.Select(d => d.Z).Min()
+                        || droplet.Z >= droplets.Select(d => d.Z).Max())
+                    {
+                        droplet.type = "outside";
+                        updated++;
+                    }
+                    if (droplets.Where(d => d.X == droplet.X - 1 && d.Y == droplet.Y && d.Z == droplet.Z && d.type == "outside").Any()
+                    || droplets.Where(d => d.X == droplet.X + 1 && d.Y == droplet.Y && d.Z == droplet.Z && d.type == "outside").Any()
+                    || droplets.Where(d => d.X == droplet.X && d.Y == droplet.Y - 1 && d.Z == droplet.Z && d.type == "outside").Any()
+                    || droplets.Where(d => d.X == droplet.X && d.Y == droplet.Y + 1 && d.Z == droplet.Z && d.type == "outside").Any()
+                    || droplets.Where(d => d.X == droplet.X && d.Y == droplet.Y && d.Z == droplet.Z - 1 && d.type == "outside").Any()
+                    || droplets.Where(d => d.X == droplet.X && d.Y == droplet.Y && d.Z == droplet.Z + 1 && d.type == "outside").Any())
+                    {
+                        droplet.type = "outside";
+                        updated++;
+                    }
+                }
+            }
+
+            droplets = droplets.Where(d => d.type != "outside").ToList();
+        }
+
+        public class Droplet
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+            public int Z { get; set; }
+            public int ExposedSides { get; set; }
+            public string type { get; set; }
         }
     }
+
 }
--- AdventOfCode2022Tests/UnitTestDay18.cs
+++ AdventOfCode2022Tests/UnitTestDay18.cs
@@ -1,4 +1,5 @@
 using AdventOfCode2022;
+using Common;
 
 namespace AdventOfCode2022Tests
 {
@@ -6,10 +7,28 @@ namespace AdventOfCode2022Tests
 	public class UnitTestDay18
 	{
 		private Day18 day;
-		[TestInitialize]
+		private string data;
+		private  List<string> testData;
+        [TestInitialize]
 		public void Init()
 		{
-			day = new Day18(true);
+			data = @"2,2,2
+1,2,2
+3,2,2
+2,1,2
+2,3,2
+2,2,1
+2,2,3
+2,2,4
+2,2,6
+1,2,5
+3,2,5
+2,1,5
+2,3,5";
+
+			testData = data.SplitOnNewline();
+
+			day = new Day18(data);
 		}
 
 
@@ -17,16 +36,21 @@ namespace AdventOfCode2022Tests
 		[TestCategory("Example data")]
 		public void Part1()
 		{
+			day.Parse(testData);
+			day.FindNeigbors();
 
-			Assert.AreEqual(1, 1);
+			Assert.AreEqual(64, day.CountExposed());
 		}
 
 		[TestMethod("Day 18, Part 2")]
 		[TestCategory("Example data")]
 		public void Part2()
 		{
+            day.Parse(testData);
+			day.AddVoid();
+            day.FindNeigbors();
 
-			Assert.AreEqual(2, 2);
-		}
+            Assert.AreEqual(58, day.CountExposed());
+        }
 	}
 }
