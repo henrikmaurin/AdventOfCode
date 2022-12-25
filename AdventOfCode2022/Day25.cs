1fdb083d1223d3958d6aa694e69cdcbbf10cd516
Henrik Maurin <henrik.maurin@soprasteria.com>
2022-12-25 07:19:29 +0100

    2022, Day 25 - Done

--- AdventOfCode2022/Day25.cs
+++ AdventOfCode2022/Day25.cs
@@ -1,4 +1,5 @@
 ﻿using Common;
+using System.Text;
 
 namespace AdventOfCode2022
 {
@@ -6,28 +7,123 @@ namespace AdventOfCode2022
     {
         private const int day = 25;
         List<string> data;
-        public Day25(bool runtests = false) : base(Global.Year, day, runtests)
+        public Day25(string testdata = null) : base(Global.Year, day, testdata != null)
         {
-            if (runtests)
+            if (testdata != null)
+            {
+                data = testdata.SplitOnNewline();
                 return;
+            }
 
             data = input.GetDataCached().SplitOnNewline();
         }
         public void Run()
         {
-            int result1 = Problem1();
+            string result1 = Problem1();
             Console.WriteLine($"P1: Result: {result1}");
 
             int result2 = Problem2();
             Console.WriteLine($"P2: Result: {result2}");
         }
-        public int Problem1()
-        {
-            return 0;
+        public string Problem1()
+        {            
+            return GetSum();
         }
         public int Problem2()
         {
             return 0;
         }
+
+        public string GetSum()
+        {
+            long sum = data.Select(d => FromSnafu(d)).Sum();
+
+            return ToSnafu(sum);
+        }
+
+        public long FromSnafu(string number)
+        {
+            long multiplier = 1;
+            long sum = 0;
+            for (int i = number.Length - 1; i >= 0; i--)
+            {
+                sum += SnafuDigit(number[i]) * multiplier;
+                multiplier *= 5;
+            }
+            return sum;
+        }
+
+        public string ToSnafu(long number)
+        {            
+            string retString = "";
+
+            while (number != 0)
+            {
+                long curDigit =(long) number % 5;
+                if (curDigit.In(0,1,2))
+                {
+                    retString += $"{curDigit}";
+                    number -= curDigit;
+                }
+                else if (curDigit== 3)
+                {
+                    retString += "=";
+                    number += 2;
+                }
+                else if (curDigit== 4)
+                {
+                    retString+= "-";
+                    number += 1;
+                }
+                number /= 5;
+            }
+
+            retString = new string(retString.Reverse().ToArray());
+
+            return retString;
+        }       
+
+        public char ToSnafuDigt(int number)
+        {
+            switch (number)
+            {
+                case 2:
+                    return '2';
+                case 1:
+                    return '1';
+                case 0:
+                    return '0';
+                case -1:
+                    return '-';
+                case -2:
+                    return '=';
+            }
+            return 'a';
+
+        }
+
+        public int SnafuDigit(char digit)
+        {
+            switch (digit)
+            {
+                case '2':
+                    return 2;
+                case '1':
+                    return 1;
+                case '0':
+                    return 0;
+                case '-':
+                    return -1;
+                case '=':
+                    return -2;
+            }
+
+            return int.MaxValue;
+        }
+
+
+
+
+
     }
 }
--- AdventOfCode2022Tests/UnitTestDay25.cs
+++ AdventOfCode2022Tests/UnitTestDay25.cs
@@ -1,4 +1,5 @@
 using AdventOfCode2022;
+using Common;
 
 namespace AdventOfCode2022Tests
 {
@@ -6,10 +7,26 @@ namespace AdventOfCode2022Tests
 	public class UnitTestDay25
 	{
 		private Day25 day;
-		[TestInitialize]
+        private string data;
+        private List<string> testdata;
+        [TestInitialize]
 		public void Init()
 		{
-			day = new Day25(true);
+            data = @"1=-0-2
+12111
+2=0=
+21
+2=01
+111
+20012
+112
+1=-1=
+1-12
+12
+1=
+122";
+            testdata = data.SplitOnNewline();
+            day = new Day25(data);
 		}
 
 
@@ -17,9 +34,17 @@ namespace AdventOfCode2022Tests
 		[TestCategory("Example data")]
 		public void Part1()
 		{
-
-			Assert.AreEqual(1, 1);
-		}
+			Assert.AreEqual(1747, day.FromSnafu(testdata[0]));
+            Assert.AreEqual(906, day.FromSnafu(testdata[1]));
+            Assert.AreEqual(198, day.FromSnafu(testdata[2]));
+            Assert.AreEqual(11, day.FromSnafu(testdata[3]));
+            Assert.AreEqual(201, day.FromSnafu(testdata[4]));
+            Assert.AreEqual(31, day.FromSnafu(testdata[5]));
+            Assert.AreEqual(1257, day.FromSnafu(testdata[6]));
+          
+            Assert.AreEqual(testdata[0], day.ToSnafu(1747));
+            Assert.AreEqual("1121-1110-1=0", day.ToSnafu(314159265));
+        }
 
 		[TestMethod("Day 25, Part 2")]
 		[TestCategory("Example data")]
