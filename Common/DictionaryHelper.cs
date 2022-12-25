785c8bcd82f4b59f21ae82d3c2ced43f84c11679
Henrik Maurin <henrik.maurin@soprasteria.com>
2022-12-25 11:53:46 +0100

    DictionaryHelper

---
+++ Common/DictionaryHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public static class DictionaryHelper
+    {
+        public static void AddOrUpdate<TKey, TValue>(this Dictionary<TKey, TValue> dict, TKey key, TValue value)
+        {
+            if (dict.ContainsKey(key))
+                dict[key] = value;
+            else 
+                dict.Add(key, value);
+        }
+    }
+}
