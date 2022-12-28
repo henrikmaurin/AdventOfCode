76c9b5438d69cf3de01a03e70f67631843ae60e6
Henrik Maurin <henrik.maurin@soprasteria.com>
2022-12-28 15:28:37 +0100

    2018, Day 9, Tests, slight refactor

--- AdventOfCode2018Tests/UnitTestDay08.cs
+++ AdventOfCode2018Tests/UnitTestDay08.cs
@@ -4,37 +4,34 @@ using Common;
 
 namespace Tests
 {
-	[TestClass]
-	public class UnitTestDay08
-	{
-		private Day08 day;
+    [TestClass]
+    public class UnitTestDay08
+    {
+        private Day08 day;
         private string data;
-        private string[] testdata;
 
         [TestInitialize]
         public void Init()
         {
-            data = @"";
-            testdata = data.SplitOnNewlineArray();
+            data = @"2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2";
+           
 
-            day = new Day08(data);
-		}
+            day = new Day08(data);         
+        }
 
 
-		[TestMethod("Day 8, Part 1")]
-		[TestCategory("Example data")]
-		public void Part1()
-		{
+        [TestMethod("Day 8, Part 1")]
+        [TestCategory("Example data")]
+        public void Part1()
+        {
+            Assert.AreEqual(138, day.Problem1());
+        }
 
-			Assert.AreEqual(1, 1);
-		}
-
-		[TestMethod("Day 8, Part 2")]
-		[TestCategory("Example data")]
-		public void Part2()
-		{
-
-			Assert.AreEqual(2, 2);
-		}
-	}
+        [TestMethod("Day 8, Part 2")]
+        [TestCategory("Example data")]
+        public void Part2()
+        {
+            Assert.AreEqual(66, day.Problem2());
+        }
+    }
 }
