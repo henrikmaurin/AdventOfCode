6c7d9b64e965022fe55b2cc5d39cc4b37d3e4dd7
Henrik Maurin <henrik.maurin@soprasteria.com>
2022-12-30 14:29:02 +0100

    Fix day 13

--- AdventOfCode2018Tests/UnitTestDay13.cs
+++ AdventOfCode2018Tests/UnitTestDay13.cs
@@ -4,37 +4,50 @@ using Common;
 
 namespace Tests
 {
-	[TestClass]
-	public class UnitTestDay13
-	{
-		private Day13 day;
+    [TestClass]
+    public class UnitTestDay13
+    {
+        private Day13 day;
         private string data;
         private string[] testdata;
 
         [TestInitialize]
         public void Init()
         {
-            data = @"";
+            data = @"/->-\        
+|   |  /----\
+| /-+--+-\  |
+| | |  | v  |
+\-+-/  \-+--/
+  \------/   ";
             testdata = data.SplitOnNewlineArray(false);
 
             day = new Day13(data);
-		}
+        }
 
 
-		[TestMethod("Day 13, Part 1")]
-		[TestCategory("Example data")]
-		public void Part1()
-		{
+        [TestMethod("Day 13, Part 1")]
+        [TestCategory("Example data")]
+        public void Part1()
+        {
+            day.InitMap();
+            Assert.AreEqual("7,3", day.RunUntilCrash().ToString());
+        }
 
-			Assert.AreEqual(1, 1);
-		}
-
-		[TestMethod("Day 13, Part 2")]
-		[TestCategory("Example data")]
-		public void Part2()
-		{
-
-			Assert.AreEqual(2, 2);
-		}
-	}
+        [TestMethod("Day 13, Part 2")]
+        [TestCategory("Example data")]
+        public void Part2()
+        {
+            data = @"/>-<\  
+|   |  
+| /<+-\
+| | | v
+\>+</ |
+  |   ^
+  \<->/";
+            day = new Day13(data);
+            day.InitMap();
+            Assert.AreEqual("6,4", day.RunUntilLastCrash().ToString());
+        }
+    }
 }
