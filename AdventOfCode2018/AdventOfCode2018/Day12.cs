60eadc01ef2792daaeb11e3734b3484591985848
Henrik Maurin <henrik.maurin@soprasteria.com>
2022-12-30 12:26:57 +0100

    2018, Day 12, Tests and refactorize

--- AdventOfCode2018/AdventOfCode2018/Day12.cs
+++ AdventOfCode2018/AdventOfCode2018/Day12.cs
@@ -1,5 +1,6 @@
 ﻿using Common;
 using Common;
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,16 +11,12 @@ namespace AdventOfCode2018
     {
         private const int day = 12;
         private string[] data;
-        //  public char[,] Pots { get; set; }
         public char[] Pots { get; set; }
         public Dictionary<string, char> Rules { get; set; }
 
-        private ulong iterations = 20;
-        private ulong longiterations = 160;
         private int size = 1000;
         private int startingpoint = 5;
-        private string[] lines;
-
+ 
         public Day12(string testdata = null) : base(Global.Year, day, testdata != null)
         {
             if (testdata != null)
@@ -27,15 +24,35 @@ namespace AdventOfCode2018
                 data = testdata.SplitOnNewlineArray();
                 return;
             }
-            lines = input.GetDataCached().SplitOnNewlineArray();
+            data = input.GetDataCached().SplitOnNewlineArray();
+        }
+
+        public void Run()
+        {
+            ulong result1 = Problem1();
+            Console.WriteLine($"P1: {result1}");
+
+            ulong result2 = Problem2();
+            Console.WriteLine($"P2: {result2}");
+        }
+
+        public ulong Problem1()
+        {
+            InitPots();
+            return GrowPots(20);
+        }
+
+        public ulong Problem2()
+        {
+            InitPots();
+            return GrowPots(50000000000);
         }
 
         public void InitPots()
         {
-            //Pots = new char[longiterations + 1, size];
             Pots = new char[size];
 
-            string initial = lines[0].Replace("initial state: ", "");
+            string initial = data[0].Replace("initial state: ", "");
             int pos = startingpoint;
 
             for (int i = 0; i < size; i++)
@@ -48,30 +65,20 @@ namespace AdventOfCode2018
 
             Rules = new Dictionary<string, char>();
 
-            for (int i = 1; i < lines.Count(); i++)
+            for (int i = 1; i < data.Count(); i++)
             {
-                string Key = lines[i].Split(" => ")[0].Trim();
-                char Value = lines[i].Split(" => ")[1].Trim()[0];
+                string Key = data[i].Split(" => ")[0].Trim();
+                char Value = data[i].Split(" => ")[1].Trim()[0];
 
                 Rules.Add(Key, Value);
             }
         }
 
-        public void Run()
+        public ulong GrowPots(ulong iterations)
         {
-            string result1 = Problem1();
-            Console.WriteLine($"P1: {result1}");
-
-            string result2 = Problem2();
-            Console.WriteLine($"P2: {result2}");
-        }
-
-        public string Problem1()
-        {
-            InitPots();
-
             char[] lastPots = Pots;
             char[] pots = new char[size];
+            ulong repeatPos = iterations + 1;
 
             for (ulong i = 0; i < iterations; i++)
             {
@@ -85,43 +92,7 @@ namespace AdventOfCode2018
                 {
 
                     string potCombo = "" + lastPots[pos - 2] + lastPots[pos - 1] + lastPots[pos] + lastPots[pos + 1] + lastPots[pos + 2];
-                    char newVal = Rules[potCombo];
-                    pots[pos] = newVal;
-                }
-                lastPots = pots;
-            }
-
-            ulong sum = 0;
-
-            for (int i = 0; i < size; i++)
-            {
-                sum += pots[i] == '#' ? (ulong)(i - startingpoint) : 0;
-
-            }
-
-            return $"Pot Sum: {sum}";
-        }
-
-        public string Problem2()
-        {
-            InitPots();
-            char[] lastPots = Pots;
-            char[] pots = new char[size];
-            ulong repeatPos = 0;
-
-            for (ulong i = 0; i < longiterations; i++)
-            {
-                pots = new char[size];
-                pots[0] = '.';
-                pots[1] = '.';
-                pots[size - 1] = '.';
-                pots[size - 2] = '.';
-
-                for (int pos = 2; pos < size - 2; pos++)
-                {
-
-                    string potCombo = "" + lastPots[pos - 2] + lastPots[pos - 1] + lastPots[pos] + lastPots[pos + 1] + lastPots[pos + 2];
-                    char newVal = Rules[potCombo];
+                    char newVal = Rules.TryGetValue(potCombo, out char value) ? value : '.';
                     pots[pos] = newVal;
                 }
                 if (EqualAsLast(lastPots, pots))
@@ -130,8 +101,6 @@ namespace AdventOfCode2018
                     break;
                 }
                 lastPots = pots;
-
-
             }
 
             ulong sum = 0;
@@ -151,10 +120,9 @@ namespace AdventOfCode2018
 
             ulong difference = sum - lastButOneSum;
 
-            ulong bigsum = sum + (50000000000 - repeatPos - 1) * difference;
+            ulong bigsum = sum + (iterations - repeatPos - 1) * difference;
 
-            return $"Difference per line: {difference} gives total sum of {bigsum}";
-
+            return bigsum;
         }
 
 
--- AdventOfCode2018Tests/UnitTestDay12.cs
+++ AdventOfCode2018Tests/UnitTestDay12.cs
@@ -4,37 +4,44 @@ using Common;
 
 namespace Tests
 {
-	[TestClass]
-	public class UnitTestDay12
-	{
-		private Day12 day;
+    [TestClass]
+    public class UnitTestDay12
+    {
+        private Day12 day;
         private string data;
         private string[] testdata;
 
         [TestInitialize]
         public void Init()
         {
-            data = @"";
+            data = @"initial state: #..#.#..##......###...###
+
+...## => #
+..#.. => #
+.#... => #
+.#.#. => #
+.#.## => #
+.##.. => #
+.#### => #
+#.#.# => #
+#.### => #
+##.#. => #
+##.## => #
+###.. => #
+###.# => #
+####. => #";
             testdata = data.SplitOnNewlineArray(false);
 
             day = new Day12(data);
-		}
+        }
 
 
-		[TestMethod("Day 12, Part 1")]
-		[TestCategory("Example data")]
-		public void Part1()
-		{
-
-			Assert.AreEqual(1, 1);
-		}
-
-		[TestMethod("Day 12, Part 2")]
-		[TestCategory("Example data")]
-		public void Part2()
-		{
-
-			Assert.AreEqual(2, 2);
-		}
-	}
+        [TestMethod("Day 12, Part 1")]
+        [TestCategory("Example data")]
+        public void Part1()
+        {
+            day.InitPots();
+            Assert.AreEqual((ulong)325,day.GrowPots(20));
+        }     
+    }
 }
