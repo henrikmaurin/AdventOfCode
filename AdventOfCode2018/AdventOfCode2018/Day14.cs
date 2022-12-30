185ff255b723dcf9d6806ba2842d339587cfea0c
Henrik Maurin <henrik.maurin@soprasteria.com>
2022-12-30 14:24:16 +0100

    Fix Day 11

--- AdventOfCode2018/AdventOfCode2018/Day14.cs
+++ AdventOfCode2018/AdventOfCode2018/Day14.cs
@@ -1,5 +1,6 @@
 ﻿using Common;
 using Common;
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,75 +10,83 @@ namespace AdventOfCode2018
     public class Day14 : DayBase, IDay
     {
         private const int day = 14;
-        private string[] data;
         public List<int> RecipieScores { get; set; }
-        public int Rounds { get; set; }
+        public string data { get; set; }
         public Day14(string testdata = null) : base(Global.Year, day, testdata != null)
         {
             if (testdata != null)
             {
-                data = testdata.SplitOnNewlineArray();
+                data = testdata.IsSingleLine();
                 return;
             }
-            Rounds = input.GetDataCached().IsSingleLine().ToInt();
+            data = input.GetDataCached().IsSingleLine();
 
         }
 
         public void Run()
         {
-            string result1 = Problem1();
-            Console.WriteLine($"P1: Recipie: {result1}");
+            long result1 = Problem1();
+            Console.WriteLine($"P1: Recipie: {result1.ToString().PadLeft(10, '0')}");
 
             int result2 = Problem2();
             Console.WriteLine($"P2: Recipies: {result2}");
         }
-        public string Problem1()
+        public long Problem1()
         {
-            int[] elfpos = new int[2];
-            elfpos[0] = 0;
-            elfpos[1] = 1;
-
-            RecipieScores = new List<int>();
-
-            RecipieScores.Add(3);
-            RecipieScores.Add(7);
-
-
-            for (int i = 0; i < Rounds + 10; i++)
-            {
-                int sum = RecipieScores[elfpos[0]] + RecipieScores[elfpos[1]];
-                string digits = "" + sum;
-                foreach (char digit in digits.ToCharArray())
-                {
-
-                    RecipieScores.Add(int.Parse("" + digit));
-                }
-                elfpos[0] = (elfpos[0] + RecipieScores[elfpos[0]] + 1) % RecipieScores.Count;
-                elfpos[1] = (elfpos[1] + RecipieScores[elfpos[1]] + 1) % RecipieScores.Count;
-
-            }
-
-            string result = string.Empty;
-            for (int i = Rounds; i < Rounds + 10; i++)
-            {
-                result += RecipieScores[i];
-            }
-
-            return result;
+            return CalculateRecipieScores(data.ToInt());
         }
 
         public int Problem2()
         {
+            return FindRecipie(data);
+        }
+
+        public long CalculateRecipieScores(int rounds)
+        {
             int[] elfpos = new int[2];
             elfpos[0] = 0;
             elfpos[1] = 1;
 
-            RecipieScores = new List<int>();
+            RecipieScores = new List<int>
+            {
+                3,
+                7
+            };
 
-            RecipieScores.Add(3);
-            RecipieScores.Add(7);
+            for (int i = 0; i < rounds + 10; i++)
+            {
+                int sum = RecipieScores[elfpos[0]] + RecipieScores[elfpos[1]];
+                string digits = $"{sum}";
+                foreach (char digit in digits)
+                {
+                    RecipieScores.Add(digit.ToInt());
+                }
+                elfpos[0] = (elfpos[0] + RecipieScores[elfpos[0]] + 1) % RecipieScores.Count;
+                elfpos[1] = (elfpos[1] + RecipieScores[elfpos[1]] + 1) % RecipieScores.Count;
+            }
 
-            int[] goalmatrix = $"{Rounds}".ToCharArray().Select(c => c.ToInt()).ToArray();
+            string result = string.Empty;
+            for (int i = rounds; i < rounds + 10; i++)
+            {
+                result += RecipieScores[i];
+            }
+
+            return result.ToLong();
+        }
+
+        public int FindRecipie(string targetRecipie)
+        {
+            int[] elfpos = new int[2];
+            elfpos[0] = 0;
+            elfpos[1] = 1;
+
+            RecipieScores = new List<int>
+            {
+                3,
+                7
+            };
+
+            int[] goalmatrix = $"{targetRecipie}".ToCharArray().Select(c => c.ToInt()).ToArray();
             bool found = false;
             int extra = 0;
             while (!found)
@@ -92,28 +101,26 @@ namespace AdventOfCode2018
                 elfpos[0] = (elfpos[0] + RecipieScores[elfpos[0]] + 1) % RecipieScores.Count;
                 elfpos[1] = (elfpos[1] + RecipieScores[elfpos[1]] + 1) % RecipieScores.Count;
 
-                if (RecipieScores.Count > 6)
+                if (RecipieScores.Count > goalmatrix.Length)
                 {
-                    if (RecipieScores[RecipieScores.Count - 6] == goalmatrix[0] && RecipieScores[RecipieScores.Count - 5] == goalmatrix[1] && RecipieScores[RecipieScores.Count - 4] == goalmatrix[2] && RecipieScores[RecipieScores.Count - 3] == goalmatrix[3] && RecipieScores[RecipieScores.Count - 2] == goalmatrix[4] && RecipieScores[RecipieScores.Count - 1] == goalmatrix[5])
+                    bool foundfirst = true;
+                    bool foundsecond = true;
+                    int pos = 0;
+                    while (pos < goalmatrix.Length && (foundfirst || foundsecond))
                     {
-                        found = true;
+                        if (RecipieScores[RecipieScores.Count - goalmatrix.Length + pos] != goalmatrix[pos])
+                            foundfirst = false;
+                        if (RecipieScores[RecipieScores.Count - 1 - goalmatrix.Length + pos] != goalmatrix[pos])
+                            foundsecond = false;
+                        pos++;
                     }
-                    if (RecipieScores[RecipieScores.Count - 7] == goalmatrix[0] && RecipieScores[RecipieScores.Count - 6] == goalmatrix[1] && RecipieScores[RecipieScores.Count - 5] == goalmatrix[2] && RecipieScores[RecipieScores.Count - 4] == goalmatrix[3] && RecipieScores[RecipieScores.Count - 3] == goalmatrix[4] && RecipieScores[RecipieScores.Count - 2] == goalmatrix[5])
-                    {
+                    if (foundfirst || foundsecond)
                         found = true;
+                    if (foundsecond)
                         extra = -1;
-                    }
                 }
-
-
-
             }
-
-
-            // Console.WriteLine($"Recipies: {RecipieScores.Count-6+extra}");
-            return RecipieScores.Count - 6 + extra;
+            return RecipieScores.Count - goalmatrix.Length + extra;
         }
-
-
     }
 }
--- AdventOfCode2018Tests/UnitTestDay14.cs
+++ AdventOfCode2018Tests/UnitTestDay14.cs
@@ -4,10 +4,10 @@ using Common;
 
 namespace Tests
 {
-	[TestClass]
-	public class UnitTestDay14
-	{
-		private Day14 day;
+    [TestClass]
+    public class UnitTestDay14
+    {
+        private Day14 day;
         private string data;
         private string[] testdata;
 
@@ -18,23 +18,27 @@ namespace Tests
             testdata = data.SplitOnNewlineArray(false);
 
             day = new Day14(data);
-		}
+        }
 
 
-		[TestMethod("Day 14, Part 1")]
-		[TestCategory("Example data")]
-		public void Part1()
-		{
+        [TestMethod("Day 14, Part 1")]
+        [TestCategory("Example data")]
+        public void Part1()
+        {
+            Assert.AreEqual(5158916779, day.CalculateRecipieScores(9));
+            Assert.AreEqual(0124515891, day.CalculateRecipieScores(5));
+            Assert.AreEqual(9251071085, day.CalculateRecipieScores(18));
+            Assert.AreEqual(5941429882, day.CalculateRecipieScores(2018));
+        }
 
-			Assert.AreEqual(1, 1);
-		}
-
-		[TestMethod("Day 14, Part 2")]
-		[TestCategory("Example data")]
-		public void Part2()
-		{
-
-			Assert.AreEqual(2, 2);
-		}
-	}
+        [TestMethod("Day 14, Part 2")]
+        [TestCategory("Example data")]
+        public void Part2()
+        {
+            Assert.AreEqual(9, day.FindRecipie("51589"));
+            Assert.AreEqual(5, day.FindRecipie("01245"));
+            Assert.AreEqual(18, day.FindRecipie("92510"));
+            Assert.AreEqual(2018, day.FindRecipie("59414"));
+        }
+    }
 }
