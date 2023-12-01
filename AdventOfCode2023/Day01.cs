57961a355bff33d7fc6fd8cc4cfced4381050428
Henrik Maurin <henrik.maurin@soprasteria.com>
2023-12-01 13:07:22 +0100

    Day 1

--- AdventOfCode2023/Day01.cs
+++ AdventOfCode2023/Day01.cs
@@ -1,36 +1,98 @@
 ﻿using Common;
 
+using static Common.Parser;
+
 namespace AdventOfCode2023
 {
     public class Day01 : DayBase, IDay
     {
         private const int day = 1;
-        List<string> data;
+
+        SingleStrings calibrationDocument = new SingleStrings();
+
+        string data;
         public Day01(string? testdata = null) : base(Global.Year, day, testdata != null)
         {
             if (testdata != null)
             {
-                data = testdata.SplitOnNewline();
+                data = testdata;
                 return;
             }
 
-            data = input.GetDataCached().SplitOnNewline();
+            data = input.GetDataCached();
+            calibrationDocument.Lines = Parser.ParseLinesDelimitedByNewlineSingleString(data);
         }
+
         public void Run()
         {
             int result1 = Problem1();
-            Console.WriteLine($"P1: Result: {result1}");
+            Console.WriteLine($"P1: Sum of all decoded calibration values are {Answer(result1)}");
 
             int result2 = Problem2();
-            Console.WriteLine($"P2: Result: {result2}");
+            Console.WriteLine($"P2: Sum of all decoded calibration values including spelled out numbers are {Answer(result2)}");
         }
         public int Problem1()
         {
-            return 0;
+            DecoderElf elf = new DecoderElf();
+            int sum = elf.DecodeAllLines( calibrationDocument );
+
+            return sum;
         }
         public int Problem2()
         {
-            return 0;
+            DecoderElf elf = new DecoderElf();
+            int sum = elf.DecodeAllLines(calibrationDocument,alsoConvertSpelledOutNumbers:true);
+
+            return sum;
+        }
+
+        public class DecoderElf
+        {
+            public int DecodeAllLines(IEnumerable<string> linesOfCalibrationData, bool alsoConvertSpelledOutNumbers = false)
+            {
+                int sum = 0;
+                foreach (string line in linesOfCalibrationData)
+                {
+                    string lineToDecode = line;
+
+                    if (alsoConvertSpelledOutNumbers)
+                        lineToDecode = DocumentDecoder.ReplaceWordNumbers(lineToDecode);
+
+                    sum += DocumentDecoder.Findfirst(lineToDecode) * 10 + DocumentDecoder.FindLast(lineToDecode);
+                }
+                return sum;
+            }
+        }
+
+        public static class DocumentDecoder
+        {
+            private static readonly char[] numbers = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+            private static readonly string[] numberstrings = new string[] { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+            private static readonly string[] newnumbers = new string[] { "z0ro", "o1e", "t2o", "t3ree", "f4ur", "f5ve", "s6x", "s7ven", "e8ght", "n9ne" };
+
+            public static int Findfirst(string s)
+            {
+                int first = s.IndexOfAny(numbers);
+
+                return s[first].ToInt();
+            }
+
+            public static int FindLast(string s)
+            {
+                int first = s.LastIndexOfAny(numbers);
+
+                return s[first].ToInt();
+            }
+
+            public static string ReplaceWordNumbers(string s)
+            {
+                for (int i = 1; i < numberstrings.Length; i++)
+                {
+                    s = s.Replace(numberstrings[i], newnumbers[i]);
+                }
+
+                return s;
+            }
         }
     }
 }
--- AdventOfCode2023Tests/UnitTestDay01.cs
+++ AdventOfCode2023Tests/UnitTestDay01.cs
@@ -1,39 +1,79 @@
 using AdventOfCode2023;
 using Common;
 
+using static AdventOfCode2023.Day01;
+
 namespace Tests
 {
 	[TestClass]
 	public class UnitTestDay01
 	{
-		private Day01 day; 
+		//private Day01 day;
 		private string data;
-        private string[] testdata;
+		private string[] testdata;
 
-        [TestInitialize]
-        public void Init()
-        {
-            data = @"";
-            testdata = data.SplitOnNewlineArray(false);
+		[TestInitialize]
+		public void Init()
+		{
+			//data = @"";
+			//testdata = data.SplitOnNewlineArray(false);
 
-            day = new Day01(data);
+			//day = new Day01(data);
 		}
 
 
 		[TestMethod("Day 1, Part 1")]
 		[TestCategory("Example data")]
-		public void Part1()
+		[DataRow(12, "1abc2")]
+		[DataRow(38, "pqr3stu8vwx")]
+		[DataRow(15, "a1b2c3d4e5f")]
+		[DataRow(77, "treb7uchet")]
+		public void Part1_Examples(int result, string data)
 		{
+			int sum = DocumentDecoder.Findfirst(data) * 10 + DocumentDecoder.FindLast(data);
 
-			Assert.AreEqual(1, 1);
+			Assert.AreEqual(result, sum);
+		}
+
+		[TestMethod("Day 1, Part 1")]
+		[TestCategory("Example data")]		
+		public void Part1_Examples_Sum()
+		{
+			List<string> documentLines = new List<string> { "1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet" };
+			DecoderElf elf = new DecoderElf();
+			
+			int sum = elf.DecodeAllLines(documentLines);
+
+			Assert.AreEqual(142, sum);
 		}
 
 		[TestMethod("Day 1, Part 2")]
 		[TestCategory("Example data")]
-		public void Part2()
+		[DataRow(29, "two1nine")]
+		[DataRow(83, "eightwothree")]
+		[DataRow(13, "abcone2threexyz")]
+		[DataRow(24, "xtwone3four")]
+		[DataRow(42, "4nineeightseven2")]
+		[DataRow(14, "zoneight234")]
+		[DataRow(76, "7pqrstsixteen")]
+		public void Part2_Examples(int result, string data)
 		{
+			data = DocumentDecoder.ReplaceWordNumbers(data);
+			int sum = DocumentDecoder.Findfirst(data) * 10 + DocumentDecoder.FindLast(data);
 
-			Assert.AreEqual(2, 2);
+			Assert.AreEqual(result, sum);
+		}
+
+		[TestMethod("Day 1, Part 1")]
+		[TestCategory("Example data")]
+		public void Part2_Examples_Sum()
+		{
+			List<string> documentLines = new List<string> { "two1nine", "eightwothree", "abcone2threexyz", "xtwone3four", "4nineeightseven2", "zoneight234" , "7pqrstsixteen" };
+			DecoderElf elf = new DecoderElf();
+
+			int sum = elf.DecodeAllLines(documentLines,true);
+
+			Assert.AreEqual(281, sum);
 		}
 	}
 }
