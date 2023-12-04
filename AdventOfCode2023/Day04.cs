3451f8bcadc85eb862ee434e1e1b3b7ff8ad9f11
Henrik Maurin <henrik.maurin@soprasteria.com>
2023-12-04 07:56:14 +0100

    Initial solution Day 4

--- AdventOfCode2023/Day04.cs
+++ AdventOfCode2023/Day04.cs
@@ -18,19 +18,80 @@ namespace AdventOfCode2023
         }
         public void Run()
         {
-            int result1 = Problem1();
+
+
+            double result1 = Problem1();
             Console.WriteLine($"P1: Result: {result1}");
 
             int result2 = Problem2();
             Console.WriteLine($"P2: Result: {result2}");
         }
-        public int Problem1()
+        public double Problem1()
         {
-            return 0;
+            double result = 0;
+
+            foreach (var item in data)
+            {
+                string numbers = item.Split(": ").Last();
+
+                int[] winningNumbers = numbers.Trim().Split('|').First().Trim().Split(' ').Select(s => s.ToInt()).ToArray();
+                int[] cardnumbers = numbers.Trim().Split('|').Last().Trim().Replace("  ", " ").Split(' ').Select(s => s.ToInt()).ToArray();
+
+
+                int numberofhits = 0;
+                foreach (var number in cardnumbers)
+                {
+                    if (number.In(winningNumbers))
+                        numberofhits++;
+                }
+
+
+                if (numberofhits > 0)
+                {
+                    result += Math.Pow(2, numberofhits - 1);
+                }
+            }
+            return result;
         }
         public int Problem2()
         {
-            return 0;
+            int result = 0;
+
+            Queue<string> queue = new Queue<string>();
+
+
+            foreach (var item in data)
+                queue.Enqueue(item);
+
+
+            while (queue.Count > 0)
+            {
+                string item = queue.Dequeue();
+                result++;
+
+                int gameno = item.Split(":").First().Split(" ").Last().ToInt();
+                string numbers = item.Split(": ").Last();
+
+                int[] winningNumbers = numbers.Trim().Split('|').First().Trim().Split(' ').Select(s => s.ToInt()).ToArray();
+                int[] cardnumbers = numbers.Trim().Split('|').Last().Trim().Replace("  ", " ").Split(' ').Select(s => s.ToInt()).ToArray();
+
+
+                int numberofhits = 0;
+                foreach (var number in cardnumbers)
+                {
+                    if (number.In(winningNumbers))
+                        numberofhits++;
+                }
+
+                for (int i = 0; i < numberofhits; i++)
+                {
+                    if (i +gameno < data.Count)
+                    {
+                        queue.Enqueue(data[i+gameno]);
+                    }
+                }
+            }
+            return result;
         }
     }
 }
