09d37072a1009adf072f1ba942998543ec00e305
Henrik Maurin <henrik.maurin@soprasteria.com>
2023-12-07 21:51:56 +0100

    Refactor day 6, tests

--- AdventOfCode2023/Day06.cs
+++ AdventOfCode2023/Day06.cs
@@ -1,6 +1,4 @@
-﻿using System.Diagnostics;
-
-using Common;
+﻿using Common;
 
 namespace AdventOfCode2023
 {
@@ -8,6 +6,7 @@ namespace AdventOfCode2023
     {
         private const int day = 6;
         List<string> data;
+        List<RaceTime> racetimes;
         public Day06(string? testdata = null) : base(Global.Year, day, testdata != null)
         {
             if (testdata != null)
@@ -17,13 +16,21 @@ namespace AdventOfCode2023
             }
 
             data = input.GetDataCached().SplitOnNewline();
+            int[] times = data[0].SplitOnWhitespace().Skip(1).ToArray().ToInt();
+            int[] distances = data[1].SplitOnWhitespace().Skip(1).ToArray().ToInt();
+
+            racetimes = new List<RaceTime>();
+            for (int i = 0; i < times.Length; i++)
+            {
+                racetimes.Add(new RaceTime { Time = times[i], Distance = distances[i] });
+            }
         }
         public void Run()
         {
-            int result1 = MeasureExecutionTime(() => Problem1());    
+            int result1 = MeasureExecutionTime(() => Problem1());
             WriteAnswer(1, "The multiplied values of button presses is {result}", result1);
 
-            long result2 = MeasureExecutionTime(()=> Problem2());
+            long result2 = MeasureExecutionTime(() => Problem2());
             WriteAnswer(2, "There is a total {result} ways to win", result2);
         }
 
@@ -38,58 +45,100 @@ namespace AdventOfCode2023
             return 0;
         }
 
+        public class RaceTime
+        {
+            public int Time { get; set; }
+            public long Distance { get; set; }
+        }
+
+        public static class RaceElf
+        {
+            public static int RunAllRaces(IEnumerable<RaceTime> races)
+            {
+                int total = 1;
+                foreach (RaceTime raceTime in races)
+                {
+                    total *= RaceBoat.TryAllVariants(raceTime.Time, raceTime.Distance);
+                }
+                return total;
+            }
+
+            public static int UseMaths(IEnumerable<RaceTime> races)
+            {
+                int total = 1;
+                foreach (RaceTime raceTime in races)
+                {
+                    total *= UseMaths(raceTime);
+                }
+                return total;
+            }
+
+            public static int UseMaths(RaceTime raceTime)
+            {
+                double halftime = (double)raceTime.Time / 2;
+                double halftimesquared = Math.Pow(halftime, 2);
+
+                double x1 = halftime + Math.Sqrt(halftimesquared - raceTime.Distance);
+                double x2 = halftime - Math.Sqrt(halftimesquared - raceTime.Distance);
+
+                decimal t1 = Math.Floor((decimal)x1);
+                decimal t2 = Math.Ceiling((decimal)x2);
+
+                if (x1%1==0)
+                    t1--;
+                if (x2 % 1 == 0)
+                    t2++;
+
+                double to = (double)(t1 - t2)+1;
+                return (int)to;
+            }
+
+            public static List<RaceTime> FixKerning(IEnumerable<RaceTime> raceTimes)
+            {
+                string correctTime = string.Empty;
+                string correctDistance = string.Empty;
+                foreach (RaceTime raceTime in raceTimes)
+                {
+                    correctTime += $"{raceTime.Time}";
+                    correctDistance += $"{raceTime.Distance}";
+                }
+                return new List<RaceTime> { new RaceTime { Time = correctTime.ToInt(), Distance = correctDistance.ToLong() } };
+            }
+        }
+        public static class RaceBoat
+        {
+            public static int TryBeat(int buttonTime, int maxtime, long distanceToBeat)
+            {
+                long timeForRace = maxtime - buttonTime;
+
+                if (timeForRace * buttonTime > distanceToBeat)
+                {
+                    return 1;
+                }
+                return 0;
+            }
+
+            public static int TryAllVariants(int maxtime, long distanceToBeat)
+            {
+                int sum = 0;
+                for (int t = 1; t <= maxtime; t++)
+                {
+                    sum += TryBeat(t, maxtime, distanceToBeat);
+                }
+                return sum;
+            }
+
+        }
 
         public int Problem1()
         {
-            int[] times = data[0].SplitOnWhitespace().Skip(1).ToArray().ToInt();
-            int[] distances = data[1].SplitOnWhitespace().Skip(1).ToArray().ToInt();
-
-            //times = new int[] { 30 };
-            //distances = new int[] { 200 };
-
-            int total = 1;
-
-            for (int i = 0; i < times.Length; i++)
-            {
-                int sum = 0;
-                for (int t = 1; t <= times[i]; t++)
-                {
-                    sum += Win(t, times[i], distances[i]);
-                }
-                total *= sum;
-            }
-            return total;
+            return RaceElf.UseMaths(racetimes);
+            //return RaceElf.RunAllRaces(racetimes);
         }
         public long Problem2()
         {
-            int time = data[0].Replace("Time:", "").Replace(" ", "").ToInt();
-            long distance = data[1].Replace("Distance:", "").Replace(" ", "").ToLong();
-
-/*
-            double halftime = time / 2;
-            double halftimesquared = Math.Pow(halftime, 2);
-
-
-            double x1 = halftime + Math.Sqrt(halftimesquared - distance);
-            double x2 = halftime - Math.Sqrt(halftimesquared - distance);
-
-            decimal t1 = Math.Ceiling((decimal)x1);
-            decimal t2 = Math.Ceiling((decimal)x2);
-
-            double to = (double)(t1-t2);
-*/
-            //time = 71530;
-            //distance = 940200;
-
-            long total = 1;
-            int sum = 0;
-            for (int t = 1; t <= time; t++)
-            {
-                sum += Win(t, time, distance);
-            }
-            total *= sum;
-
-            return sum;
+            return RaceElf.UseMaths(RaceElf.FixKerning(racetimes).First());
+            //return RaceElf.RunAllRaces(RaceElf.FixKerning(racetimes));
         }
     }
 }
--- AdventOfCode2023Tests/UnitTestDay06.cs
+++ AdventOfCode2023Tests/UnitTestDay06.cs
@@ -1,39 +1,41 @@
-using AdventOfCode2023;
-using Common;
+using static AdventOfCode2023.Day06;
 
 namespace Tests
 {
-	[TestClass]
-	public class UnitTestDay06
-	{
-		private Day06 day;
-		private string data;
-		private string[] testdata;
+    [TestClass]
+    public class UnitTestDay06
+    {
+        [TestInitialize]
+        public void Init()
+        {
 
-		[TestInitialize]
-		public void Init()
-		{
-			data = @"";
-			testdata = data.SplitOnNewlineArray(false);
-
-			day = new Day06(data);
-		}
+        }
 
 
-		[TestMethod("Day 6, Part 1")]
-		[TestCategory("Example data")]
-		public void Part1()
-		{
+        [TestMethod("Day 6, Part 1")]
+        [TestCategory("Example data")]
+        [DataRow(4, 7, 9)]
+        [DataRow(8, 15, 40)]
+        [DataRow(9, 30, 200)]
+        public void Part1(int expectedResult, int time, int distance)
+        {
+            int result = RaceBoat.TryAllVariants(time, distance);
+            int result2 = RaceElf.UseMaths(new RaceTime { Time = time, Distance = distance });
 
-			Assert.AreEqual(1, 1);
-		}
+            Assert.AreEqual(expectedResult, result);
+            Assert.AreEqual(expectedResult, result2);
+        }
 
-		[TestMethod("Day 6, Part 2")]
-		[TestCategory("Example data")]
-		public void Part2()
-		{
+        [TestMethod("Day 6, Part 2")]
+        [TestCategory("Example data")]
+        [DataRow(71503, 71530, 940200)]
+        public void Part2(int expectedResult, int time, int distance)
+        {
+            int result = RaceBoat.TryAllVariants(time, distance);
+            int result2 = RaceElf.UseMaths(new RaceTime { Time = time, Distance = distance });
 
-			Assert.AreEqual(2, 2);
-		}
-	}
+            Assert.AreEqual(expectedResult, result);
+            Assert.AreEqual(expectedResult, result2);
+        }
+    }
 }
