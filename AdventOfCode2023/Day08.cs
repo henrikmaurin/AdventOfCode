6a2ca89c4be78425974d14f9c736aace1ce5dd06
Henrik Maurin <henrik.maurin@soprasteria.com>
2023-12-09 13:53:08 +0100

    Refactor day 8

--- AdventOfCode2023/Day08.cs
+++ AdventOfCode2023/Day08.cs
@@ -6,6 +6,10 @@ namespace AdventOfCode2023
     {
         private const int day = 8;
         List<string> data;
+
+        private string Instructions;
+        private CollectionOfNodes Nodes;
+
         public Day08(string? testdata = null) : base(Global.Year, day, testdata != null)
         {
             if (testdata != null)
@@ -15,139 +19,123 @@ namespace AdventOfCode2023
             }
 
             data = input.GetDataCached().SplitOnNewline();
-        }
-        public void Run()
-        {
-            int result1 = MeasureExecutionTime(()=> Problem1());
-            WriteAnswer(1,"{result} steps needed to reach ZZZ", result1);
-
-            long result2 = MeasureExecutionTime(()=> Problem2());
-            WriteAnswer(2, "{result} steps needed for all nodes to reach ??Z", result2);
-            
-        }
-        public int Problem1()
-        {
-            
-
-            string instructions = data.First();
-
-            Dictionary<string,NetworkNode> nodes = new Dictionary<string,NetworkNode>();
-
-            for (int i=1;i<data.Count;i++)
-            {
-                string key = data[i].Split(" = ").First();
-                string left = data[i].Split(" = ").Last().Split(", ").First().Replace("(", "");
-                string right = data[i].Split(" = ").Last().Split(", ").Last().Replace(")", "");
-                nodes.Add(key,new NetworkNode { Left = left, Right = right });
-
-            }
-
-            string currentnode = "AAA";
-            int counter = 0;
-            while (currentnode != "ZZZ")
-            {
-                char instruction = instructions[counter % instructions.Length];
-                if (instruction == 'L')
-                    currentnode = nodes[currentnode].Left;
-                if (instruction == 'R')
-                    currentnode = nodes[currentnode].Right;
-
-
-                counter++;
-            }
-
-
-
-
-
-
-            return counter;
-        }
-        public long Problem2()
-        {
-            string instructions = data.First();
-
-            Dictionary<string, NetworkNode> nodes = new Dictionary<string, NetworkNode>();
+            Instructions = data.First();
+            Nodes = new CollectionOfNodes();
 
             for (int i = 1; i < data.Count; i++)
             {
                 string key = data[i].Split(" = ").First();
                 string left = data[i].Split(" = ").Last().Split(", ").First().Replace("(", "");
                 string right = data[i].Split(" = ").Last().Split(", ").Last().Replace(")", "");
-                nodes.Add(key, new NetworkNode { Left = left, Right = right });
-
+                Nodes.Nodes.Add(key, new NetworkNode { Left = left, Right = right });
             }
 
-            string[] currentnodes = nodes.Where(n => n.Key.EndsWith("A")).Select(n=>n.Key).ToArray();
-            long counter = 0;
+        }
+        public void Run()
+        {
+            int result1 = MeasureExecutionTime(() => Problem1());
+            WriteAnswer(1, "{result} steps needed to reach ZZZ", result1);
 
-            Dictionary<int,long> keyValuePairs = new Dictionary<int,long>();
-            Dictionary<int,long> repeats = new Dictionary<int,long>();            
+            long result2 = MeasureExecutionTime(() => Problem2());
+            WriteAnswer(2, "{result} steps needed for all nodes to reach ??Z", result2);
 
-            while (currentnodes.Where(n => n.EndsWith("Z")).Count() < currentnodes.Length)
-            {
-                for (int i = 0; i < currentnodes.Length; i++)
-                {
-                    int pos =(int) (counter % (long)instructions.Length);
-
-
-                    char instruction = instructions[pos];
-                    if (instruction == 'L')
-                        currentnodes[i] = nodes[currentnodes[i]].Left;
-                    if (instruction == 'R')
-                        currentnodes[i] = nodes[currentnodes[i]].Right;
-
-                    if (currentnodes[i].EndsWith("Z"))
-                    {
-                        
-                        long currentcount = 1;
-
-                        if ((keyValuePairs.ContainsKey(i)))
-                        { currentcount = keyValuePairs[i];
-                            keyValuePairs[i] = counter;
-                            if (!repeats .ContainsKey(i))
-                            {
-                                repeats.Add(i, counter - currentcount);
-                            }
-                        }
-                        else
-                        {
-                            keyValuePairs.Add(i, counter);
-                        }
-                        Console.WriteLine($"{i} {counter - currentcount}");
-
-                        long diff = counter - currentcount;
-                       if (repeats.Count() == currentnodes.Length)
-                        {   
-                            long result = repeats.Select(r=>r.Value).ElementAt(0);
-                            for (int j=1; j < currentnodes.Length; j++)
-                            {
-                                result = MathHelpers.LeastCommonMultiple(result, repeats.Select(r => r.Value).ElementAt(j));
-                            }
-
-                            return result;
-                        }
-                        
-
-                    }
-                }
-
-               
-
-                counter++;
-            }
-
-
-
-
-
-
-            return counter;
+        }
+        public int Problem1()
+        {
+            return MapReader.FollowInstructions(Instructions, Nodes);
         }
 
-        class NetworkNode
+
+        public long Problem2()
         {
-            public string Left  { get; set; }
+            return MapReader.FollowGhostInstructions(Instructions, Nodes);            
+        }
+
+        public static class MapReader
+        {
+            public static int FollowInstructions(string mapInstructions, CollectionOfNodes nodeCollection)
+            {
+                string currentnode = "AAA";
+                int counter = 0;
+                while (currentnode != "ZZZ")
+                {
+                    char instruction = mapInstructions[counter % mapInstructions.Length];
+                    currentnode = nodeCollection.GetNextNode(currentnode, instruction);
+
+                    counter++;
+                }
+                return counter;
+            }
+
+            public static long FollowGhostInstructions(string mapInstructions, CollectionOfNodes nodeCollection)
+            {
+                string[] currentnodes = nodeCollection.GetGhostStartingPoints();
+                long counter = 0;
+
+                Dictionary<int, long> repeatSteps = new Dictionary<int, long>();
+      
+                while (currentnodes.Where(n => n.EndsWith("Z")).Count() < currentnodes.Length)
+                {
+                    for (int i = 0; i < currentnodes.Length; i++)
+                    {
+                        int pos = (int)(counter % (long)mapInstructions.Length);
+                        char instruction = mapInstructions[pos];
+
+                        currentnodes[i] = nodeCollection.GetNextNode(currentnodes[i], instruction);
+
+
+                        if (currentnodes[i].EndsWith("Z"))
+                        {
+                            long currentcount = 0;
+
+                            if (!repeatSteps.ContainsKey(i))
+                            {
+                                repeatSteps.Add(i, counter + 1);
+                            }
+
+                            if (repeatSteps.Count() == currentnodes.Length)
+                            {
+                                long result = repeatSteps.Select(r => r.Value).ElementAt(0);
+                                for (int j = 1; j < currentnodes.Length; j++)
+                                {
+                                    result = MathHelpers.LeastCommonMultiple(result, repeatSteps.Select(r => r.Value).ElementAt(j));
+                                }
+
+                                return result;
+                            }
+                        }
+                    }
+                    counter++;
+                }
+                return counter;
+            }
+        }
+
+        public class CollectionOfNodes
+        {
+            public CollectionOfNodes()
+            {
+                Nodes = new Dictionary<string, NetworkNode>();
+            }
+            public Dictionary<string, NetworkNode> Nodes { get; set; }
+            public string GetNextNode(string node, char instruction)
+            {
+                if (instruction == 'L')
+                    return Nodes[node].Left;
+                if (instruction == 'R')
+                    return Nodes[node].Right;
+                throw new Exception("Instruction not supported");
+            }
+            public string[] GetGhostStartingPoints()
+            {
+                return Nodes.Where(n => n.Key.EndsWith("A")).Select(n => n.Key).ToArray(); ;
+            }
+
+        }
+
+        public class NetworkNode
+        {
+            public string Left { get; set; }
             public string Right { get; set; }
         }
     }
--- AdventOfCode2023Tests/UnitTestDay08.cs
+++ AdventOfCode2023Tests/UnitTestDay08.cs
@@ -1,6 +1,8 @@
 using AdventOfCode2023;
 using Common;
 
+using static AdventOfCode2023.Day08;
+
 namespace Tests
 {
     [TestClass]
@@ -22,18 +24,38 @@ namespace Tests
 
         [TestMethod("Day 8, Part 1")]
         [TestCategory("Example data")]
-        public void Part1()
+        public void Part1_Example()
         {
+            string instructions = "LLR";
+            CollectionOfNodes  nodes = new CollectionOfNodes();
+            nodes.Nodes.Add("AAA", new NetworkNode { Left = "BBB", Right = "BBB" });
+            nodes.Nodes.Add("BBB", new NetworkNode { Left = "AAA", Right = "ZZZ" });
+            nodes.Nodes.Add("ZZZ", new NetworkNode { Left = "ZZZ", Right = "ZZZ" });
 
-            Assert.AreEqual(1, 1);
+            int result = MapReader.FollowInstructions(instructions, nodes);
+
+
+            Assert.AreEqual(6, result);
         }
 
         [TestMethod("Day 8, Part 2")]
         [TestCategory("Example data")]
-        public void Part2()
+        public void Part2_Example()
         {
+            string instructions = "LR";
+            CollectionOfNodes nodes = new CollectionOfNodes();
+            nodes.Nodes.Add("11A", new NetworkNode { Left = "11B", Right = "XXX" });
+            nodes.Nodes.Add("11B", new NetworkNode { Left = "XXX", Right = "11Z" });
+            nodes.Nodes.Add("11Z", new NetworkNode { Left = "11B", Right = "XXX" });
+            nodes.Nodes.Add("22A", new NetworkNode { Left = "22B", Right = "XXX" });
+            nodes.Nodes.Add("22B", new NetworkNode { Left = "22C", Right = "22C" });
+            nodes.Nodes.Add("22C", new NetworkNode { Left = "22Z", Right = "22Z" });
+            nodes.Nodes.Add("22Z", new NetworkNode { Left = "22B", Right = "22B" });
+            nodes.Nodes.Add("XXX", new NetworkNode { Left = "ZZZ", Right = "ZZZ" });
 
-            Assert.AreEqual(2, 2);
+            long result = MapReader.FollowGhostInstructions(instructions, nodes);
+
+            Assert.AreEqual(6, result);
         }
     }
 }
