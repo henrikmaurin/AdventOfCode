1833c3a0e88164eb717d0dbf287f50b919c45f52
Henrik Maurin <henrik.maurin@soprasteria.com>
2024-12-01 06:14:12 +0100

    Initail sollution Day 01

--- AdventOfCode2024/Day01.cs
+++ AdventOfCode2024/Day01.cs
@@ -15,6 +15,10 @@ namespace AdventOfCode2024
             }
 
             data = input.GetDataCached().SplitOnNewline();
+
+
+            
+
         }
         public void Run()
         {
@@ -26,11 +30,48 @@ namespace AdventOfCode2024
         }
         public int Problem1()
         {
-            return 0;
+            List<int> numbers1 = new List<int>();
+            List<int> numbers2 = new List<int>();
+
+
+            foreach (var item in data)
+            {
+                numbers1.Add(int.Parse(item.Split(' ').First()));
+                numbers2.Add(int.Parse(item.Split(' ').Last()));
+            }
+
+            numbers1.Sort();
+            numbers2.Sort();
+
+            int sum = 0;
+
+            for (int i = 0; i < numbers1.Count; i++)
+            {
+               sum += Math.Abs(  numbers1[i] - numbers2[i]);
+            }
+
+            return sum;
+
         }
         public int Problem2()
         {
-            return 0;
+            List<int> numbers1 = new List<int>();
+            List<int> numbers2 = new List<int>();
+
+            foreach (var item in data)
+            {
+                numbers1.Add(int.Parse(item.Split(' ').First()));
+                numbers2.Add(int.Parse(item.Split(' ').Last()));
+            }
+         
+            int sum = 0;
+
+            foreach (var item in numbers1)
+            {
+                sum += item * numbers2.Where(n => n == item).Count();
+            }
+
+            return sum;
         }
     }
 }
--- AdventOfCode2024Tests/UnitTestDay01.cs
+++ AdventOfCode2024Tests/UnitTestDay01.cs
@@ -3,11 +3,11 @@ using Common;
 
 namespace Tests
 {
-	[TestClass]
-	public class UnitTestDay01
-	{
-		private Day01 day; 
-		private string data;
+    [TestClass]
+    public class UnitTestDay01
+    {
+        private Day01 day; 
+        private string data;
         private string[] testdata;
 
         [TestInitialize]
@@ -17,23 +17,23 @@ namespace Tests
             testdata = data.SplitOnNewlineArray(false);
 
             day = new Day01(data);
-		}
+        }
 
 
-		[TestMethod("Day 1, Part 1")]
-		[TestCategory("Example data")]
-		public void Part1()
-		{
+        [TestMethod("Day 1, Part 1")]
+        [TestCategory("Example data")]
+        public void Part1()
+        {
 
-			Assert.AreEqual(1, 1);
-		}
+            Assert.AreEqual(1, 1);
+        }
 
-		[TestMethod("Day 1, Part 2")]
-		[TestCategory("Example data")]
-		public void Part2()
-		{
+        [TestMethod("Day 1, Part 2")]
+        [TestCategory("Example data")]
+        public void Part2()
+        {
 
-			Assert.AreEqual(2, 2);
-		}
-	}
+            Assert.AreEqual(2, 2);
+        }
+    }
 }
