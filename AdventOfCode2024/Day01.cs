531b0c3e963afe42dbe61179e8d3f8cdf31544e5
Henrik Maurin <henrik.maurin@soprasteria.com>
2024-12-01 06:31:51 +0100

    Day 1

--- AdventOfCode2024/Day01.cs
+++ AdventOfCode2024/Day01.cs
@@ -6,69 +6,66 @@ namespace AdventOfCode2024
     {
         private const int day = 1;
         List<string> data;
+
+        private List<int> List1;
+        private List<int> List2;
+
+
         public Day01(string? testdata = null) : base(Global.Year, day, testdata != null)
         {
             if (testdata != null)
             {
                 data = testdata.SplitOnNewline();
+                Parse();
                 return;
             }
 
             data = input.GetDataCached().SplitOnNewline();
-
-
-            
-
+            Parse();
         }
+
+        public void Parse()
+        {
+            List1 = new List<int>();
+            List2 = new List<int>();
+
+            foreach (var item in data)
+            {
+                List1.Add(int.Parse(item.Split(' ').First()));
+                List2.Add(int.Parse(item.Split(' ').Last()));
+            }
+        }
+
         public void Run()
         {
             int result1 = MeasureExecutionTime(() => Problem1());
-            WriteAnswer(1, "Result: {result}", result1);
+            WriteAnswer(1, "Total distance between list is {result}", result1);
 
             int result2 = MeasureExecutionTime(() => Problem2());
-            WriteAnswer(2, "Result: {result}", result2);
+            WriteAnswer(2, "The similarity score of the lista is {result}", result2);
         }
         public int Problem1()
         {
-            List<int> numbers1 = new List<int>();
-            List<int> numbers2 = new List<int>();
-
-
-            foreach (var item in data)
-            {
-                numbers1.Add(int.Parse(item.Split(' ').First()));
-                numbers2.Add(int.Parse(item.Split(' ').Last()));
-            }
-
-            numbers1.Sort();
-            numbers2.Sort();
+            List<int> locationIds1 = List1.OrderBy(n => n).ToList();
+            List<int> locationIds2 = List2.OrderBy(n => n).ToList();
 
             int sum = 0;
 
-            for (int i = 0; i < numbers1.Count; i++)
+            for (int i = 0; i < locationIds1.Count; i++)
             {
-               sum += Math.Abs(  numbers1[i] - numbers2[i]);
+                sum += Math.Abs(locationIds1[i] - locationIds2[i]);
             }
 
             return sum;
-
         }
+
         public int Problem2()
         {
-            List<int> numbers1 = new List<int>();
-            List<int> numbers2 = new List<int>();
-
-            foreach (var item in data)
-            {
-                numbers1.Add(int.Parse(item.Split(' ').First()));
-                numbers2.Add(int.Parse(item.Split(' ').Last()));
-            }
-         
             int sum = 0;
 
-            foreach (var item in numbers1)
+            foreach (var locationId in List1)
             {
-                sum += item * numbers2.Where(n => n == item).Count();
+                sum += locationId * List1.Where(n => n == locationId).Count();
             }
 
             return sum;
--- AdventOfCode2024Tests/UnitTestDay01.cs
+++ AdventOfCode2024Tests/UnitTestDay01.cs
@@ -13,7 +13,12 @@ namespace Tests
         [TestInitialize]
         public void Init()
         {
-            data = @"";
+            data = @"3   4
+4   3
+2   5
+1   3
+3   9
+3   3";
             testdata = data.SplitOnNewlineArray(false);
 
             day = new Day01(data);
@@ -24,8 +29,10 @@ namespace Tests
         [TestCategory("Example data")]
         public void Part1()
         {
+            int sum = day.Problem1();
 
-            Assert.AreEqual(1, 1);
+
+            Assert.AreEqual(11, sum);
         }
 
         [TestMethod("Day 1, Part 2")]
