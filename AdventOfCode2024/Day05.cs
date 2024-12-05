330f739e724fb7723783c0ac17c3e5fede9f6d1d
Henrik Maurin <henrik.maurin@soprasteria.com>
2024-12-05 06:54:21 +0100

    Day 5 initial, Very inefficient

--- AdventOfCode2024/Day05.cs
+++ AdventOfCode2024/Day05.cs
@@ -7,16 +7,47 @@ namespace AdventOfCode2024
         private const int day = 5;
         List<string> data;
 
+        List<NumberPair> numbers;
+        List<List<int>> orders;
+
         public Day05(string? testdata = null) : base(Global.Year, day, testdata != null)
         {
             if (testdata != null)
             {
                 data = testdata.SplitOnNewline();
+                Parse();
                 return;
             }
 
             data = input.GetDataCached().SplitOnNewline();
+            Parse();
+
         }
+
+        public void Parse()
+        {
+            numbers = new List<NumberPair>();
+            orders = new List<List<int>>();
+
+            foreach (var item in data)
+            {
+                if (item.Contains('|'))
+                {
+                    int number1 = item.Split('|').First().ToInt();
+                    int number2 = item.Split('|').Last().ToInt();
+
+                    numbers.Add(new NumberPair { Value1 = number1, Value2 = number2 });      
+                }
+
+                if (item.Contains(','))
+                {
+                    orders.Add(item.Split(',').ToInt().ToList());
+                }
+            }
+
+        }
+
+
         public void Run()
         {
             int result1 = MeasureExecutionTime(() => Problem1());
@@ -27,11 +58,83 @@ namespace AdventOfCode2024
         }
         public int Problem1()
         {
-            return 0;
+            int sum = 0;
+
+            for (int i = 0; i < orders.Count; i++)
+            {
+                bool correctOrder = Ordered(orders[i]);
+
+                if (correctOrder)
+                {
+                    sum += orders[i][orders[i].Count / 2];
+                }
+
+            }
+
+            return sum;
         }
         public int Problem2()
         {
-            return 0;
+            int sum = 0;
+
+            for (int i = 0; i < orders.Count; i++)
+            {
+                bool correctOrder = Ordered(orders[i]);
+
+                if (!correctOrder)
+                {
+                    List<int> correct = new List<int>();
+
+                    correct.Add(orders[i][0] );
+
+                    for (int j = 1; j < orders[i].Count; j++)
+                    {
+                        List<int> test = new List<int>();
+                        test.AddRange(correct);
+                        int pos = 0;
+                        test.Insert(pos, orders[i][j]);
+                        while (!Ordered(test))
+                        {
+                            pos++;
+                            test = new List<int>();
+                            test.AddRange(correct);
+                            test.Insert(pos, orders[i][j]);
+                        }
+                        correct = test;
+                    }
+
+                    sum += correct[correct.Count / 2];
+                }
+
+            }
+
+            return sum;
         }
+
+        public bool Ordered(List<int> list)
+        {        
+
+            for (int j = 0; j < list.Count ; j++)
+            {
+                foreach (NumberPair np in numbers.Where(n => n.Value1 == list[j]))
+                {
+                    if (!list.Contains(np.Value1) || !list.Contains(np.Value2))
+                    { continue; }
+
+                    if (!(list.IndexOf(np.Value2) > list.IndexOf(np.Value1)))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+
+    class NumberPair
+    {
+        public int Value1 { get; set; }
+        public int Value2 { get; set; }
     }
 }
--- AdventOfCode2024Tests/UnitTestDay05.cs
+++ AdventOfCode2024Tests/UnitTestDay05.cs
@@ -1,39 +1,67 @@
 using AdventOfCode2024;
+
 using Common;
 
 namespace Tests
 {
-	[TestClass]
-	public class UnitTestDay05
-	{
-		private Day05 day;
+    [TestClass]
+    public class UnitTestDay05
+    {
+        private Day05 day;
         private string data;
         private string[] testdata;
 
         [TestInitialize]
         public void Init()
         {
-            data = @"";
+            data = @"47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47";
             testdata = data.SplitOnNewlineArray(false);
 
             day = new Day05(data);
-		}
+        }
 
 
-		[TestMethod("Day 5, Part 1")]
-		[TestCategory("Example data")]
-		public void Part1()
-		{
+        [TestMethod("Day 5, Part 1")]
+        [TestCategory("Example data")]
+        public void Part1()
+        {
+            int result = day.Problem1();
+            Assert.AreEqual(143, result);
+        }
 
-			Assert.AreEqual(1, 1);
-		}
-
-		[TestMethod("Day 5, Part 2")]
-		[TestCategory("Example data")]
-		public void Part2()
-		{
-
-			Assert.AreEqual(2, 2);
-		}
-	}
+        [TestMethod("Day 5, Part 2")]
+        [TestCategory("Example data")]
+        public void Part2()
+        {
+            int result = day.Problem2();
+            Assert.AreEqual(123, result);
+        }
+    }
 }
