ff56ee3273deab8103b60b61b1ee8c4863e8f9fa
Henrik Maurin <henrik.maurin@soprasteria.com>
2024-12-07 06:51:09 +0100

    Day 7, recursion

--- AdventOfCode2024/Day07.cs
+++ AdventOfCode2024/Day07.cs
@@ -1,36 +1,175 @@
 ﻿using Common;
 
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
 namespace AdventOfCode2024
 {
     public class Day07 : DayBase, IDay
     {
         private const int day = 7;
         List<string> data;
+        List<Data> numbers;
+
+
         public Day07(string? testdata = null) : base(Global.Year, day, testdata != null)
         {
             if (testdata != null)
             {
                 data = testdata.SplitOnNewline();
+                Parse();
                 return;
             }
 
             data = input.GetDataCached().SplitOnNewline();
+            Parse();
+        }
+
+        public void Parse()
+        {
+            numbers = new List<Data>();
+            foreach (string line in data)
+            {
+                string[] s1 = line.Split(':');
+                Data data1 = new Data();
+                data1.Result = s1[0].ToLong();
+                data1.Numbers = s1[1].Trim().Split(" ").ToLong().ToList();
+                numbers.Add(data1);
+
+            }
         }
         public void Run()
         {
-            int result1 = MeasureExecutionTime(() => Problem1());
+            long result1 = MeasureExecutionTime(() => Problem1());
             WriteAnswer(1, "Result: {result}", result1);
 
-            int result2 = MeasureExecutionTime(() => Problem2());
+            long result2 = MeasureExecutionTime(() => Problem2());
             WriteAnswer(2, "Result: {result}", result2);
         }
-        public int Problem1()
+
+        public bool Mulitply(long number, List<long> numbers, long result, bool useConcat = false)
         {
-            return 0;
+            if (numbers.Count == 1)
+            {
+                if (number * numbers[0] == result)
+                    return true;
+
+                return false;
+            }
+
+            bool r = Mulitply(number * numbers[0], numbers[1..], result,useConcat);
+            if (r)
+                return true;
+            r = Add(number * numbers[0], numbers[1..], result,useConcat);
+            if (r)
+                return true;
+            if (useConcat)
+                r = Concat(number * numbers[0], numbers[1..], result);
+
+            return r;
         }
-        public int Problem2()
+
+        public bool Add(long number, List<long> numbers, long result, bool useConcat = false)
         {
-            return 0;
+            if (numbers.Count == 1)
+            {
+                if (number + numbers[0] == result)
+                    return true;
+
+                return false;
+            }
+
+            bool r = Mulitply(number + numbers[0], numbers[1..], result,useConcat);
+            if (r)
+                return true;
+            r = Add(number + numbers[0], numbers[1..], result, useConcat);
+            if (r)
+                return true;
+            if (useConcat)
+                r = Concat(number + numbers[0], numbers[1..], result);
+
+            return r;
+        }
+
+        public bool Concat(long number, List<long> numbers, long result)
+        {
+            long n = $"{number}{numbers[0]}".ToLong();
+
+            if (numbers.Count == 1)
+            {
+                if (n == result)
+                    return true;
+
+                return false;
+            }
+
+            bool r = Mulitply(n, numbers[1..], result,true);
+            if (r)
+                return true;
+            r = Add(n, numbers[1..], result,true);
+            if (r)
+                return true;
+            r = Concat(n, numbers[1..], result);
+
+            return r;
+        }
+
+
+
+        public long Problem1()
+        {
+            long sum = 0;
+            foreach (Data d in numbers)
+            {
+                bool r = Mulitply(d.Numbers[0], d.Numbers[1..], d.Result);
+                if (r)
+                {
+                    sum += d.Result;
+                    continue;
+                }
+                r = Add(d.Numbers[0], d.Numbers[1..], d.Result);
+                if (r)
+                {
+                    sum += d.Result;
+                }
+
+            }
+
+
+            return sum;
+        }
+        public long Problem2()
+        {
+            long sum = 0;
+            foreach (Data d in numbers)
+            {
+                bool r = Mulitply(d.Numbers[0], d.Numbers[1..], d.Result, useConcat: true);
+                if (r)
+                {
+                    sum += d.Result;
+                    continue;
+                }
+                r = Add(d.Numbers[0], d.Numbers[1..], d.Result, useConcat: true);
+                if (r)
+                {
+                    sum += d.Result;
+                    continue;
+                }
+                r = Concat(d.Numbers[0], d.Numbers[1..], d.Result);
+                if (r)
+                {
+                    sum += d.Result;
+                    continue;
+                }
+
+            }
+           
+            return sum;
+        }
+
+        public class Data
+        {
+            public long Result { get; set; }
+            public List<long> Numbers { get; set; }
         }
     }
 }
--- AdventOfCode2024Tests/UnitTestDay07.cs
+++ AdventOfCode2024Tests/UnitTestDay07.cs
@@ -1,39 +1,48 @@
 using AdventOfCode2024;
+
 using Common;
 
 namespace Tests
 {
-	[TestClass]
-	public class UnitTestDay07
-	{
-		private Day07 day;
+    [TestClass]
+    public class UnitTestDay07
+    {
+        private Day07 day;
         private string data;
         private string[] testdata;
 
         [TestInitialize]
         public void Init()
         {
-            data = @"";
+            data = @"190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20";
             testdata = data.SplitOnNewlineArray(false);
 
             day = new Day07(data);
-		}
+        }
 
 
-		[TestMethod("Day 7, Part 1")]
-		[TestCategory("Example data")]
-		public void Part1()
-		{
+        [TestMethod("Day 7, Part 1")]
+        [TestCategory("Example data")]
+        public void Part1()
+        {
+            long result = day.Problem1();
+            Assert.AreEqual(3749, result);
+        }
 
-			Assert.AreEqual(1, 1);
-		}
-
-		[TestMethod("Day 7, Part 2")]
-		[TestCategory("Example data")]
-		public void Part2()
-		{
-
-			Assert.AreEqual(2, 2);
-		}
-	}
+        [TestMethod("Day 7, Part 2")]
+        [TestCategory("Example data")]
+        public void Part2()
+        {
+            long result = day.Problem2();
+            Assert.AreEqual(11387, result);
+        }
+    }
 }
