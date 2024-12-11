ddfd6b49317350349562544777fe10faee066fe7
Henrik Maurin <henrik.maurin@soprasteria.com>
2024-12-11 06:57:47 +0100

    Day 11 initial, brute force 1st, recursion with memoisation part 2.

--- AdventOfCode2024/Day11.cs
+++ AdventOfCode2024/Day11.cs
@@ -1,36 +1,122 @@
-﻿using Common;
+﻿using System.Security.Cryptography.X509Certificates;
+
+using Common;
 
 namespace AdventOfCode2024
 {
     public class Day11 : DayBase, IDay
     {
         private const int day = 11;
-        List<string> data;
+        string data;
         public Day11(string? testdata = null) : base(Global.Year, day, testdata != null)
         {
             if (testdata != null)
             {
-                data = testdata.SplitOnNewline();
+                data = testdata.IsSingleLine();
+                Parse();
                 return;
             }
 
-            data = input.GetDataCached().SplitOnNewline();
+            data = input.GetDataCached().IsSingleLine();
+            Parse();
+        }
+
+        public void Parse()
+        {
+
         }
         public void Run()
         {
-            int result1 = MeasureExecutionTime(() => Problem1());
+            long result1 = MeasureExecutionTime(() => Problem1());
             WriteAnswer(1, "Result: {result}", result1);
 
-            int result2 = MeasureExecutionTime(() => Problem2());
+            long result2 = MeasureExecutionTime(() => Problem2());
             WriteAnswer(2, "Result: {result}", result2);
         }
-        public int Problem1()
+        public long Problem1()
         {
-            return 0;
+            LinkedList<string> list = new LinkedList<string>();
+            foreach (string item in data.SplitOnWhitespace())
+            {
+                list.AddLast(item);
+            }
+
+            for (int i = 0; i < 25; i++)
+            {
+                LinkedListNode<string> node = list.First;
+
+                while (node != null)
+                {
+                    if (node.Value == "0")
+                    {
+                        node.Value = "1";
+                        node= node.Next;
+                        continue;
+                    }
+
+                    if (node.Value.Length%2 == 0)
+                    {
+                        string val = node.Value;
+                        node.Value = val.Substring(0, val.Length/2).ToLong().ToString();
+                        node = list.AddAfter(node,val.Substring(val.Length / 2).ToLong().ToString());
+                        node = node.Next;
+                        continue;
+                    }
+
+                    node.Value = (node.Value.ToLong() * 2024).ToString();
+                    node= node.Next;
+
+                }
+
+            }
+
+
+
+            return list.Count;
         }
-        public int Problem2()
+        public long Problem2()
         {
-            return 0;
+            long result = 0;
+            foreach (string item in data.SplitOnWhitespace())
+            {
+                result += Rules(item, 75);
+            }
+            return result;
+        }
+
+        Dictionary<string, long> values = new Dictionary<string, long>();
+
+        public long Rules(string d, int depth)
+        {
+            string key = $"{d}:{depth}";
+            if (values.ContainsKey(key))
+                return values[key];
+
+            if (depth == 0)
+                return 1;
+
+            long retVal;
+
+            if (d == "0")
+            {
+                 retVal= Rules("1", depth-1);
+                values.Add(key, retVal);
+
+                return retVal;
+            }
+
+            if (d.Length%2==0)
+            {
+                retVal = Rules(d.Substring(0,d.Length/2).ToLong().ToString(), depth-1);
+                retVal += Rules(d.Substring(d.Length / 2).ToLong().ToString(),depth-1);
+                values.Add(key , retVal);
+                return retVal;
+            }
+
+            retVal = Rules((d.ToLong() * 2024).ToString(),depth-1);
+            values.Add(key , retVal);
+
+            return retVal;
         }
     }
 }
--- AdventOfCode2024Tests/UnitTestDay11.cs
+++ AdventOfCode2024Tests/UnitTestDay11.cs
@@ -1,42 +1,41 @@
 using AdventOfCode2024;
+
 using Common;
 
 namespace Tests
 {
-	[TestClass]
-	public class UnitTestDay11
-	{
-		private Day11 day;
+    [TestClass]
+    public class UnitTestDay11
+    {
+        private Day11 day;
         private string data;
         private string[] testdata;
 
         [TestInitialize]
         public void Init()
         {
-            data = @"";
-            testdata = data.SplitOnNewlineArray(false);
+            data = @"125 17";
 
             day = new Day11(data);
-		}
+        }
 
 
-		[TestMethod("Day 11, Part 1")]
-		[TestCategory("Example data")]
-		public void Part1()
-		{
-			
+        [TestMethod("Day 11, Part 1")]
+        [TestCategory("Example data")]
+        public void Part1()
+        {
+            long result = day.Problem1();
 
+            Assert.AreEqual(55312, result);
+        }
 
+        [TestMethod("Day 11, Part 2")]
+        [TestCategory("Example data")]
+        public void Part2()
+        {
+            long result = day.Problem2();
 
-			Assert.AreEqual(1, 1);
-		}
-
-		[TestMethod("Day 11, Part 2")]
-		[TestCategory("Example data")]
-		public void Part2()
-		{
-
-			Assert.AreEqual(2, 2);
-		}
-	}
+            Assert.AreEqual(55312, result);
+        }
+    }
 }
