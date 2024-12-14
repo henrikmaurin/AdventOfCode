5d0f3277906046964861647b36082f4136380002
Henrik Maurin <henrik.maurin@soprasteria.com>
2024-12-14 13:05:15 +0100

    Day 13

--- AdventOfCode2024/Day13.cs
+++ AdventOfCode2024/Day13.cs
@@ -6,31 +6,163 @@ namespace AdventOfCode2024
     {
         private const int day = 13;
         List<string> data;
+        List<Machine> machines;
         public Day13(string? testdata = null) : base(Global.Year, day, testdata != null)
         {
             if (testdata != null)
             {
                 data = testdata.SplitOnNewline();
+                Parse();
                 return;
             }
 
             data = input.GetDataCached().SplitOnNewline();
+            Parse(); return;
         }
+
+        public void Parse()
+        {
+            machines = new List<Machine>();
+            Machine machine = new Machine();
+            foreach (var item in data)
+            {
+
+                var c = item.Split(": ").Last();
+                int x = c.Split(", ").First().Replace("X", "").Replace("=", "").ToInt();
+                int y = c.Split(", ").Last().Replace("Y", "").Replace("=", "").ToInt();
+
+
+                if (item.StartsWith("Button A:"))
+                {
+                    machine.A = new Vector2D(x, y);
+                }
+                if (item.StartsWith("Button B"))
+                {
+                    machine.B = new Vector2D(x, y);
+                }
+                if (item.StartsWith("Prize"))
+                {
+                    machine.Target = new Vector2D(x, y);
+                    machines.Add(machine);
+                    machine = new Machine();
+                }
+
+
+            }
+
+        }
+
         public void Run()
         {
-            int result1 = MeasureExecutionTime(() => Problem1());
+            long result1 = MeasureExecutionTime(() => Problem1());
             WriteAnswer(1, "Result: {result}", result1);
 
-            int result2 = MeasureExecutionTime(() => Problem2());
+            long result2 = MeasureExecutionTime(() => Problem2());
             WriteAnswer(2, "Result: {result}", result2);
         }
-        public int Problem1()
+        public long Problem1()
         {
-            return 0;
+            long result = 0;
+
+            foreach (var machine in machines)
+            {
+                long d1 = Determinant(machine.Target.X, machine.Target.Y, machine.B.X,machine.B.Y) / Determinant(machine.A.X,machine.A.Y, machine.B.X,machine.B.Y);
+                long d2 = Determinant(machine.A.X, machine.A.Y, machine.Target.X, machine.Target.Y) / Determinant(machine.A.X, machine.A.Y, machine.B.X, machine.B.Y);
+
+                if (machine.A.X * d1 + machine.B.X * d2 == machine.Target.X && machine.A.Y * d1 + machine.B.Y * d2 == machine.Target.Y)
+                {
+                    result += 3 * d1 + d2;
+                }
+            }
+
+
+
+
+
+            return result;
         }
-        public int Problem2()
+        public long Problem2()
         {
-            return 0;
+            long result = 0;
+
+            foreach (var machine in machines)
+            {
+                long tx = machine.Target.X + 10000000000000;
+                long ty = machine.Target.Y + 10000000000000;
+
+
+                long d1 = Determinant(tx, ty, machine.B.X, machine.B.Y) / Determinant(machine.A.X, machine.A.Y, machine.B.X, machine.B.Y);
+                long d2 = Determinant(machine.A.X, machine.A.Y, tx, ty) / Determinant(machine.A.X, machine.A.Y, machine.B.X, machine.B.Y);
+
+                if (machine.A.X * d1 + machine.B.X * d2 ==tx && machine.A.Y * d1 + machine.B.Y * d2 == ty)
+                {
+                    result += 3 * d1 + d2;
+                }
+            }
+
+
+
+
+
+            return result;
+        }
+
+        long Determinant(long ax,long ay,long bx, long by)
+        {
+            return ax * by - ay * bx;
+        }
+
+        static void SolveDiophantine(long t, long a, long b, out long x, out long y)
+        {
+            long gcd = GCD(a, b, out long x0, out long y0);
+            x = -1;
+            y = -1;
+
+            // Check if t is divisible by gcd(a, b)
+            if (t % gcd != 0)
+            {
+                return;
+            }
+
+            // Scale the particular solution by t / gcd
+            long scale = t / gcd;
+            x0 *= scale;
+            y0 *= scale;
+
+            x = x0;
+            y = y0;
+        }
+
+        static long GCD(long a, long b, out long x, out long y)
+        {
+            if (b == 0)
+            {
+                x = 1;
+                y = 0;
+                return a;
+            }
+
+            long gcd = GCD(b, a % b, out long x1, out long y1);
+
+            x = y1;
+            y = x1 - (a / b) * y1;
+
+            return Math.Abs(gcd);
+        }
+
+        public class Press
+        {
+            public int Steps { get; set; }
+            public int ATimes { get; set; }
+            public int BTimes { get; set; }
+            public Vector2D CurrentPos { get; set; }
+        }
+
+        public class Machine
+        {
+            public Vector2D A { get; set; }
+            public Vector2D B { get; set; }
+            public Vector2D Target { get; set; }
         }
     }
 }
--- AdventOfCode2024Tests/UnitTestDay13.cs
+++ AdventOfCode2024Tests/UnitTestDay13.cs
@@ -1,39 +1,54 @@
 using AdventOfCode2024;
+
 using Common;
 
 namespace Tests
 {
-	[TestClass]
-	public class UnitTestDay13
-	{
-		private Day13 day;
+    [TestClass]
+    public class UnitTestDay13
+    {
+        private Day13 day;
         private string data;
         private string[] testdata;
 
         [TestInitialize]
         public void Init()
         {
-            data = @"";
+            data = @"Button A: X+94, Y+34
+Button B: X+22, Y+67
+Prize: X=8400, Y=5400
+
+Button A: X+26, Y+66
+Button B: X+67, Y+21
+Prize: X=12748, Y=12176
+
+Button A: X+17, Y+86
+Button B: X+84, Y+37
+Prize: X=7870, Y=6450
+
+Button A: X+69, Y+23
+Button B: X+27, Y+71
+Prize: X=18641, Y=10279";
             testdata = data.SplitOnNewlineArray(false);
 
             day = new Day13(data);
-		}
+        }
 
 
-		[TestMethod("Day 13, Part 1")]
-		[TestCategory("Example data")]
-		public void Part1()
-		{
+        [TestMethod("Day 13, Part 1")]
+        [TestCategory("Example data")]
+        public void Part1()
+        {
+            long result = day.Problem1();
+            Assert.AreEqual(480, result);
+        }
 
-			Assert.AreEqual(1, 1);
-		}
-
-		[TestMethod("Day 13, Part 2")]
-		[TestCategory("Example data")]
-		public void Part2()
-		{
-
-			Assert.AreEqual(2, 2);
-		}
-	}
+        [TestMethod("Day 13, Part 2")]
+        [TestCategory("Example data")]
+        public void Part2()
+        {
+            long result = day.Problem2();
+            Assert.AreEqual(2, result);
+        }
+    }
 }
