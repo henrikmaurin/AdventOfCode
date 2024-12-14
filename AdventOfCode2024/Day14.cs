4c129397f36b608a51576b0e281e1a6803d260a7
Henrik Maurin <henrik.maurin@soprasteria.com>
2024-12-14 13:05:24 +0100

    Day 14

--- AdventOfCode2024/Day14.cs
+++ AdventOfCode2024/Day14.cs
@@ -6,16 +6,39 @@ namespace AdventOfCode2024
     {
         private const int day = 14;
         List<string> data;
+        List<Robot> Robots;
+        Vector2D floor;
+
+
         public Day14(string? testdata = null) : base(Global.Year, day, testdata != null)
         {
             if (testdata != null)
             {
                 data = testdata.SplitOnNewline();
+                Parse();
+                floor = new Vector2D(11, 7);
                 return;
             }
 
             data = input.GetDataCached().SplitOnNewline();
+            Parse();
+            floor = new Vector2D(101, 103);
         }
+        public void Parse()
+        {
+            Robots = new List<Robot>();
+            foreach (var item in data)
+            {
+                string pos = item.Split(" ").First().Replace("p=", "");
+                string vel = item.Split(" ").Last().Replace("v=", "");
+                Robot robot = new Robot();
+                robot.Position = new Vector2D(pos.Split(',').First().ToInt(), pos.Split(',').Last().ToInt());
+                robot.Velocity = new Vector2D(vel.Split(',').First().ToInt(), vel.Split(',').Last().ToInt());
+                Robots.Add(robot);
+            }
+        }
+
+
         public void Run()
         {
             int result1 = MeasureExecutionTime(() => Problem1());
@@ -26,10 +49,93 @@ namespace AdventOfCode2024
         }
         public int Problem1()
         {
-            return 0;
+            int result = 1;
+            for (int i = 1; i <= 4; i++)
+            {
+                int robotCount = Robots.Where(r => r.Quadrant(100, floor) == i).Count();
+                result *= robotCount;
+            }
+
+            return result;
+
         }
         public int Problem2()
         {
+            int seconds = -1;
+            while (seconds < 100000)
+            {
+                seconds++;
+                HashSet<Vector2D> set = new HashSet<Vector2D>();
+                foreach (Robot robot in Robots)
+                {
+                    set.TryAdd(robot.CalculatePosition(seconds, floor));
+                }
+                if (set.Count == 500)
+                {
+                    //Print(seconds);
+                    //Console.WriteLine(seconds);
+                    return seconds;
+                }
+
+
+            }
+
+            return seconds;
+        }
+
+        public void Print(int seconds)
+        {
+            for (int y = 0; y < floor.Y; y++)
+            {
+                for (int x = 0; x < floor.X; x++)
+                {
+                    Vector2D pos = new Vector2D(x, y);
+
+                    if (Robots.Where(r => r.CalculatePosition(seconds, floor).Equals(pos)).Any())
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(".");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+
+    public class Robot
+    {
+        public Vector2D Position { get; set; }
+        public Vector2D Velocity { get; set; }
+
+        public Vector2D CalculatePosition(int seconds, Vector2D floor)
+        {
+            return new Vector2D((Position.X + floor.X + (seconds * Velocity.X % floor.X)) % floor.X, (Position.Y + floor.Y + (seconds * Velocity.Y % floor.Y)) % floor.Y);
+        }
+
+        public int Quadrant(int seconds, Vector2D floor)
+        {
+            Vector2D position = CalculatePosition(seconds, floor);
+
+            if (position.X < floor.X / 2 && position.Y < floor.Y / 2)
+            {
+                return 1;
+            }
+            if (position.X < floor.X / 2 && position.Y > floor.Y / 2)
+            {
+                return 2;
+            }
+            if (position.X > floor.X / 2 && position.Y > floor.Y / 2)
+            {
+                return 3;
+            }
+            if (position.X > floor.X / 2 && position.Y < floor.Y / 2)
+            {
+                return 4;
+            }
+
             return 0;
         }
     }
--- AdventOfCode2024Tests/UnitTestDay14.cs
+++ AdventOfCode2024Tests/UnitTestDay14.cs
@@ -1,39 +1,93 @@
 using AdventOfCode2024;
+
 using Common;
 
 namespace Tests
 {
-	[TestClass]
-	public class UnitTestDay14
-	{
-		private Day14 day;
+    [TestClass]
+    public class UnitTestDay14
+    {
+        private Day14 day;
         private string data;
         private string[] testdata;
 
         [TestInitialize]
         public void Init()
         {
-            data = @"";
+            data = @"p=0,4 v=3,-3
+p=6,3 v=-1,-3
+p=10,3 v=-1,2
+p=2,0 v=2,-1
+p=0,0 v=1,3
+p=3,0 v=-2,-2
+p=7,6 v=-1,-3
+p=3,0 v=-1,-2
+p=9,3 v=2,3
+p=7,3 v=-1,2
+p=2,4 v=2,-3
+p=9,5 v=-3,-3";
             testdata = data.SplitOnNewlineArray(false);
 
             day = new Day14(data);
-		}
+        }
+
+        [TestMethod]
+        public void TestMovement()
+        {
+            Robot robot = new Robot();
+            Vector2D floor = new Vector2D(11, 7);
+            robot.Position = new Vector2D(2, 4);
+            robot.Velocity = new Vector2D(2, -3);
+
+            Assert.AreEqual(2, robot.Quadrant(0, floor));
+
+            Assert.AreEqual(4, robot.CalculatePosition(1, floor).X);
+            Assert.AreEqual(1, robot.CalculatePosition(1, floor).Y);
+            Assert.AreEqual(1, robot.Quadrant(1, floor));
+
+            Assert.AreEqual(6, robot.CalculatePosition(2, floor).X);
+            Assert.AreEqual(5, robot.CalculatePosition(2, floor).Y);
+            Assert.AreEqual(3, robot.Quadrant(2, floor));
+
+            Assert.AreEqual(8, robot.CalculatePosition(3, floor).X);
+            Assert.AreEqual(2, robot.CalculatePosition(3, floor).Y);
+            Assert.AreEqual(4, robot.Quadrant(3, floor));
+
+            Assert.AreEqual(10, robot.CalculatePosition(4, floor).X);
+            Assert.AreEqual(6, robot.CalculatePosition(4, floor).Y);
+            Assert.AreEqual(3, robot.Quadrant(4, floor));
+
+            Assert.AreEqual(1, robot.CalculatePosition(5, floor).X);
+            Assert.AreEqual(3, robot.CalculatePosition(5, floor).Y);
+            Assert.AreEqual(0, robot.Quadrant(5, floor));
+        }
+
+        [TestMethod]
+        public void TestMovementR0()
+        {
+            Robot robot = new Robot();
+            Vector2D floor = new Vector2D(11, 7);
+            robot.Position = new Vector2D(0, 4);
+            robot.Velocity = new Vector2D(3, -3);
+
+            Vector2D position = robot.CalculatePosition(100, floor);
+        }
 
 
-		[TestMethod("Day 14, Part 1")]
-		[TestCategory("Example data")]
-		public void Part1()
-		{
+        [TestMethod("Day 14, Part 1")]
+        [TestCategory("Example data")]
+        public void Part1()
+        {
+            int result = day.Problem1();
+            Assert.AreEqual(12, result);
+        }
 
-			Assert.AreEqual(1, 1);
-		}
+        [TestMethod("Day 14, Part 2")]
+        [TestCategory("Example data")]
+        public void Part2()
+        {
 
-		[TestMethod("Day 14, Part 2")]
-		[TestCategory("Example data")]
-		public void Part2()
-		{
-
-			Assert.AreEqual(2, 2);
-		}
-	}
+            Assert.AreEqual(2, 2);
+        }
+    }
 }
