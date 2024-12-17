ee7d5ce5beec1ffd7d63be8625ef40d04731f785
Henrik Maurin <henrik.maurin@soprasteria.com>
2024-12-17 05:56:25 +0100

    Day 16, map traversal

--- AdventOfCode2024/Day16.cs
+++ AdventOfCode2024/Day16.cs
@@ -1,36 +1,223 @@
-﻿using Common;
+﻿using System.Linq;
+
+using Common;
 
 namespace AdventOfCode2024
 {
     public class Day16 : DayBase, IDay
     {
         private const int day = 16;
-        List<string> data;
+        string[] data;
+        Map2D<char> Map;
+
+        Vector2D StartPos;
+        Vector2D EndPos;
+
         public Day16(string? testdata = null) : base(Global.Year, day, testdata != null)
         {
             if (testdata != null)
             {
-                data = testdata.SplitOnNewline();
+                data = testdata.SplitOnNewlineArray();
+                Parse();
                 return;
             }
 
-            data = input.GetDataCached().SplitOnNewline();
+            data = input.GetDataCached().SplitOnNewlineArray();
+           
+            Parse();
         }
+
+        public void Parse()
+        {
+            Map = new Map2D<char>();
+            Map.InitFromStringArray(data);
+            Map.SafeOperations = true;
+
+            foreach (Vector2D pos in Map.EnumerateCoords())
+            {
+                if (Map[pos] == 'E')
+                {
+                    EndPos = pos;
+                    Map[pos] = '.';
+                }
+                if (Map[pos] == 'S')
+                {
+                    StartPos = pos;
+                    Map[pos] = '.';
+                }
+            }
+
+
+        }
+
         public void Run()
         {
-            int result1 = MeasureExecutionTime(() => Problem1());
+            long result1 = MeasureExecutionTime(() => Problem1());
             WriteAnswer(1, "Result: {result}", result1);
 
-            int result2 = MeasureExecutionTime(() => Problem2());
+            long result2 = MeasureExecutionTime(() => Problem2());
             WriteAnswer(2, "Result: {result}", result2);
         }
-        public int Problem1()
+        public long Problem1()
         {
-            return 0;
+            long result = 0;
+
+            PriorityQueue<Step, long> steps = new PriorityQueue<Step, long>();
+            steps.Enqueue(new Step
+            {
+                Previous = null,
+                Score = 0,
+                Pos = StartPos,
+                Direction = Directions.Right,
+            },
+            0);
+
+            HashSet<(Vector2D, int)> visited = new HashSet<(Vector2D, int)>();
+
+            while (steps.Count > 0)
+            {
+                Step step = steps.Dequeue();
+                              
+                if (visited.Contains((step.Pos, step.Direction)))
+                {
+                    continue;
+                }
+
+                if (step.Pos.Equals(EndPos))
+                {
+                    return step.Score;
+                }
+
+                visited.Add((step.Pos, step.Direction));
+
+                Vector2D nextStep = step.Pos + Directions.GetDirection(step.Direction);
+
+                if (Map[nextStep] == '.')
+                {
+                    steps.Enqueue(new Step
+                    {
+                        Previous = step,
+                        Score = step.Score + 1,
+                        Pos = nextStep,
+                        Direction = step.Direction,
+                    }, step.Score + 1);
+                }
+
+                steps.Enqueue(new Step
+                {
+                    Previous = step,
+                    Score = step.Score + 1000,
+                    Direction = Directions.TurnRight(step.Direction),
+                    Pos = step.Pos,
+                }, step.Score + 1000);
+
+                steps.Enqueue(new Step
+                {
+                    Previous = step,
+                    Score = step.Score + 1000,
+                    Direction = Directions.TurnLeft(step.Direction),
+                    Pos = step.Pos,
+                }, step.Score + 1000);
+
+               
+            }
+
+
+
+            return result;
         }
-        public int Problem2()
+        public long Problem2()
         {
-            return 0;
+            long result = 0;
+
+            PriorityQueue<Step, long> steps = new PriorityQueue<Step, long>();
+            steps.Enqueue(new Step
+            {
+                Previous = null,
+                Score = 0,
+                Pos = StartPos,
+                Direction = Directions.Right,
+            },
+            0);
+
+            Dictionary<(Vector2D, int),long> visited = new Dictionary<(Vector2D, int), long>();
+
+            long bestPath = long.MaxValue;
+            HashSet<Vector2D> partOfBestPath = new HashSet<Vector2D>();
+
+
+            while (steps.Count > 0)
+            {
+                Step step = steps.Dequeue();
+
+                if (visited.ContainsKey((step.Pos, step.Direction)))
+                {
+                    if (step.Score > visited[(step.Pos, step.Direction)])
+
+                    continue;
+                }
+
+                if (step.Score > bestPath)
+                {
+                    break;
+                }
+
+                if (step.Pos.Equals(EndPos))
+                {
+                    Step backtrack = step;
+                    bestPath = step.Score;
+                    while (backtrack != null)
+                    {                       
+                        partOfBestPath.Add(backtrack.Pos);
+                        backtrack = backtrack.Previous;
+                    }
+                }
+
+                visited.AddOrUpdate((step.Pos, step.Direction),step.Score);
+
+                Vector2D nextStep = step.Pos + Directions.GetDirection(step.Direction);
+
+                if (Map[nextStep] == '.')
+                {
+                    steps.Enqueue(new Step
+                    {
+                        Previous = step,
+                        Score = step.Score + 1,
+                        Pos = nextStep,
+                        Direction = step.Direction,
+                    }, step.Score + 1);
+                }
+
+                steps.Enqueue(new Step
+                {
+                    Previous = step,
+                    Score = step.Score + 1000,
+                    Direction = Directions.TurnRight(step.Direction),
+                    Pos = step.Pos,
+                }, step.Score + 1000);
+
+                steps.Enqueue(new Step
+                {
+                    Previous = step,
+                    Score = step.Score + 1000,
+                    Direction = Directions.TurnLeft(step.Direction),
+                    Pos = step.Pos,
+                }, step.Score + 1000);
+
+
+            }
+
+
+
+            return partOfBestPath.Count;
+        }
+
+        public class Step
+        {
+            public long Score { get; set; }
+            public Vector2D Pos { get; set; }
+            public Step Previous { get; set; }
+            public int Direction { get; set; }
         }
     }
 }
--- AdventOfCode2024Tests/UnitTestDay16.cs
+++ AdventOfCode2024Tests/UnitTestDay16.cs
@@ -1,39 +1,54 @@
 using AdventOfCode2024;
+
 using Common;
 
 namespace Tests
 {
-	[TestClass]
-	public class UnitTestDay16
-	{
-		private Day16 day;
+    [TestClass]
+    public class UnitTestDay16
+    {
+        private Day16 day;
         private string data;
         private string[] testdata;
 
         [TestInitialize]
         public void Init()
         {
-            data = @"";
+            data = @"###############
+#.......#....E#
+#.#.###.#.###.#
+#.....#.#...#.#
+#.###.#####.#.#
+#.#.#.......#.#
+#.#.#####.###.#
+#...........#.#
+###.#.#####.#.#
+#...#.....#.#.#
+#.#.#.###.#.#.#
+#.....#...#.#.#
+#.###.#.#.#.#.#
+#S..#.....#...#
+###############";
             testdata = data.SplitOnNewlineArray(false);
 
             day = new Day16(data);
-		}
+        }
 
 
-		[TestMethod("Day 16, Part 1")]
-		[TestCategory("Example data")]
-		public void Part1()
-		{
+        [TestMethod("Day 16, Part 1")]
+        [TestCategory("Example data")]
+        public void Part1()
+        {
+            long result = day.Problem1();
+            Assert.AreEqual(7036, result);
+        }
 
-			Assert.AreEqual(1, 1);
-		}
-
-		[TestMethod("Day 16, Part 2")]
-		[TestCategory("Example data")]
-		public void Part2()
-		{
-
-			Assert.AreEqual(2, 2);
-		}
-	}
+        [TestMethod("Day 16, Part 2")]
+        [TestCategory("Example data")]
+        public void Part2()
+        {
+            long result = day.Problem2();
+            Assert.AreEqual(45, result);
+        }
+    }
 }
