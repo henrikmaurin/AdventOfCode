90649215c8eae82d5c8eb5cffc7bdf1671ac75ac
Henrik Maurin <henrik.maurin@soprasteria.com>
2024-12-19 05:54:54 +0100

    Day 18 BFS slow

--- AdventOfCode2024/Day18.cs
+++ AdventOfCode2024/Day18.cs
@@ -6,31 +6,213 @@ namespace AdventOfCode2024
     {
         private const int day = 18;
         List<string> data;
+        Map2D<char> Map;
         public Day18(string? testdata = null) : base(Global.Year, day, testdata != null)
         {
             if (testdata != null)
             {
                 data = testdata.SplitOnNewline();
+                Init(7, 7);
+                Parse();
                 return;
             }
 
             data = input.GetDataCached().SplitOnNewline();
+
+            Parse();
+            
+            Init(71, 71);
         }
+
+        public void Init(int x, int y)
+        {
+            Map = new Map2D<char>();
+            Map = new Map2D<char>();
+            Map.Init(x, y, '.');
+            Map.SafeOperations = true;
+        }
+
+        public void Parse()
+        {
+
+
+        }
+
         public void Run()
         {
-            int result1 = MeasureExecutionTime(() => Problem1());
+            long result1 = MeasureExecutionTime(() => Problem1());
             WriteAnswer(1, "Result: {result}", result1);
 
-            int result2 = MeasureExecutionTime(() => Problem2());
+            string result2 = MeasureExecutionTime(() => Problem2());
             WriteAnswer(2, "Result: {result}", result2);
         }
-        public int Problem1()
+        public long Problem1()
         {
-            return 0;
+            long result = 0;
+
+            for (int i = 0; i < 1024 && i < data.Count; i++)
+            {
+                int x = data[i].Split(',').First().ToInt();
+                int y = data[i].Split(',').Last().ToInt();
+                Map[x, y] = '#';
+            }
+
+            Queue<Step> steps = new Queue<Step>();
+            steps.Enqueue(new Step
+            {
+                Count = 0,
+                Coord = new Vector2D(0, 0),
+                Prevoius = null,
+            });
+
+            HashSet<Vector2D> visited = new HashSet<Vector2D>();
+
+            while (steps.Count > 0)
+            {
+                Step step = steps.Dequeue();
+
+                if (!Map.IsInRange(step.Coord))
+                {
+                    continue;
+                }
+
+                if (visited.Contains(step.Coord))
+                {
+                    continue;
+                }
+
+                visited.Add(step.Coord);
+
+                if (Map[step.Coord] == '#')
+                {
+                    continue;
+                }
+
+                if (step.Coord.X == Map.MaxX - 1 && step.Coord.Y == Map.MaxY - 1)
+                {
+                    return step.Count;
+                }
+
+                steps.Enqueue(new Step
+                {
+                    Count = step.Count + 1,
+                    Coord = step.Coord + Directions.Vector.Up,
+                    Prevoius = step
+                });
+                steps.Enqueue(new Step
+                {
+                    Count = step.Count + 1,
+                    Coord = step.Coord + Directions.Vector.Right,
+                    Prevoius = step
+                });
+                steps.Enqueue(new Step
+                {
+                    Count = step.Count + 1,
+                    Coord = step.Coord + Directions.Vector.Down,
+                    Prevoius = step
+                });
+                steps.Enqueue(new Step
+                {
+                    Count = step.Count + 1,
+                    Coord = step.Coord + Directions.Vector.Left,
+                    Prevoius = step
+                });
+            }
+
+            return result;
         }
-        public int Problem2()
+        public string Problem2()
         {
-            return 0;
+            long result = 0;
+
+            for (int i = 0; i < data.Count; i++)
+            {
+                bool reachedFinish = false;
+
+                int x = data[i].Split(',').First().ToInt();
+                int y = data[i].Split(',').Last().ToInt();
+
+                Map[x, y] = '#';
+
+                Queue<Step> steps = new Queue<Step>();
+                steps.Enqueue(new Step
+                {
+                    Count = 0,
+                    Coord = new Vector2D(0, 0),
+                    Prevoius = null,
+                });
+
+                HashSet<Vector2D> visited = new HashSet<Vector2D>();
+
+                while (steps.Count > 0)
+                {
+                    Step step = steps.Dequeue();
+
+                    if (!Map.IsInRange(step.Coord))
+                    {
+                        continue;
+                    }
+
+                    if (visited.Contains(step.Coord))
+                    {
+                        continue;
+                    }
+
+                    visited.Add(step.Coord);
+
+                    if (Map[step.Coord] == '#')
+                    {
+                        continue;
+                    }
+
+                    if (step.Coord.X == Map.MaxX - 1 && step.Coord.Y == Map.MaxY - 1)
+                    {
+                        steps = new Queue<Step>();
+                        reachedFinish = true;
+                        continue;
+                    }
+
+                    steps.Enqueue(new Step
+                    {
+                        Count = step.Count + 1,
+                        Coord = step.Coord + Directions.Vector.Up,
+                        Prevoius = step
+                    });
+                    steps.Enqueue(new Step
+                    {
+                        Count = step.Count + 1,
+                        Coord = step.Coord + Directions.Vector.Right,
+                        Prevoius = step
+                    });
+                    steps.Enqueue(new Step
+                    {
+                        Count = step.Count + 1,
+                        Coord = step.Coord + Directions.Vector.Down,
+                        Prevoius = step
+                    });
+                    steps.Enqueue(new Step
+                    {
+                        Count = step.Count + 1,
+                        Coord = step.Coord + Directions.Vector.Left,
+                        Prevoius = step
+                    });
+                }
+                if (!reachedFinish)
+                {
+                    return $"{data[i]}";
+                }
+            }
+
+
+
+            return "";
+        }
+
+        public class Step
+        {
+            public int Count { get; set; }
+            public Vector2D Coord { get; set; }
+            public Step Prevoius { get; set; }
         }
     }
 }
--- AdventOfCode2024Tests/UnitTestDay18.cs
+++ AdventOfCode2024Tests/UnitTestDay18.cs
@@ -1,39 +1,64 @@
 using AdventOfCode2024;
+
 using Common;
 
 namespace Tests
 {
-	[TestClass]
-	public class UnitTestDay18
-	{
-		private Day18 day;
+    [TestClass]
+    public class UnitTestDay18
+    {
+        private Day18 day;
         private string data;
         private string[] testdata;
 
         [TestInitialize]
         public void Init()
         {
-            data = @"";
+            data = @"5,4
+4,2
+4,5
+3,0
+2,1
+6,3
+2,4
+1,5
+0,6
+3,3
+2,6
+5,1
+1,2
+5,5
+2,5
+6,5
+1,4
+0,4
+6,4
+1,1
+6,1
+1,0
+0,5
+1,6
+2,0";
             testdata = data.SplitOnNewlineArray(false);
 
             day = new Day18(data);
-		}
+        }
 
 
-		[TestMethod("Day 18, Part 1")]
-		[TestCategory("Example data")]
-		public void Part1()
-		{
+        [TestMethod("Day 18, Part 1")]
+        [TestCategory("Example data")]
+        public void Part1()
+        {
+            long result = day.Problem1();
+            Assert.AreEqual(22, result);
+        }
 
-			Assert.AreEqual(1, 1);
-		}
-
-		[TestMethod("Day 18, Part 2")]
-		[TestCategory("Example data")]
-		public void Part2()
-		{
-
-			Assert.AreEqual(2, 2);
-		}
-	}
+        [TestMethod("Day 18, Part 2")]
+        [TestCategory("Example data")]
+        public void Part2()
+        {
+            string result = day.Problem2();
+            Assert.AreEqual("6,1", result);
+        }
+    }
 }
