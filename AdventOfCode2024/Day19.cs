32dc266050be0323d23cd779e60c8385b5b19eff
Henrik Maurin <henrik.maurin@soprasteria.com>
2024-12-19 08:06:50 +0100

    Day 19, Recursion, memoization

--- AdventOfCode2024/Day19.cs
+++ AdventOfCode2024/Day19.cs
@@ -6,6 +6,7 @@ namespace AdventOfCode2024
     {
         private const int day = 19;
         List<string> data;
+        Dictionary<string, long> seen = new Dictionary<string, long>();
         public Day19(string? testdata = null) : base(Global.Year, day, testdata != null)
         {
             if (testdata != null)
@@ -16,21 +17,114 @@ namespace AdventOfCode2024
 
             data = input.GetDataCached().SplitOnNewline();
         }
+
+        void Parse()
+        {
+
+        }
+
         public void Run()
         {
-            int result1 = MeasureExecutionTime(() => Problem1());
+            long result1 = MeasureExecutionTime(() => Problem1());
             WriteAnswer(1, "Result: {result}", result1);
 
-            int result2 = MeasureExecutionTime(() => Problem2());
+            long result2 = MeasureExecutionTime(() => Problem2());
             WriteAnswer(2, "Result: {result}", result2);
         }
-        public int Problem1()
+        public long Problem1()
         {
-            return 0;
+            long result = 0;
+
+            string[] towels = data[0].Split(", ");
+
+            foreach (string design in data.Skip(1))
+            {
+                bool r = TestDesign(design, towels);
+                if (r)
+                {
+                    result++;
+                }
+
+            }
+
+
+            return result;
         }
-        public int Problem2()
+
+        public long Problem2()
         {
-            return 0;
+            long result = 0;
+
+            string[] towels = data[0].Split(", ");
+
+            seen.Clear();
+
+            foreach (string design in data.Skip(1))
+            {
+                result += TestDesign2(design, towels, breakOnMatch: false);
+            }
+
+            return result;
         }
+
+        public bool TestDesign(string design, string[] towels)
+        {
+            if (design.Length == 0)
+            {
+
+                return true;
+            }
+
+            bool result = false;
+
+            foreach (string towel in towels)
+            {
+                if (design.StartsWith(towel))
+                {
+                    result = TestDesign(design.Substring(towel.Length), towels);
+                    if (result)
+                        break;
+                }
+
+            }
+
+            return result;
+        }
+
+
+
+        public long TestDesign2(string design, string[] towels, bool breakOnMatch)
+        {
+            if (seen.ContainsKey(design))
+            {
+                return seen[design];
+            }
+
+
+            if (design.Length == 0)
+            {
+
+                return 1;
+            }
+
+            long result = 0;
+
+            foreach (string towel in towels)
+            {
+                if (design.StartsWith(towel))
+                {
+                    result += TestDesign2(design.Substring(towel.Length), towels, breakOnMatch);
+
+                }
+
+            }
+
+            seen.Add(design, result);
+            return result;
+        }
+
+
+
+
     }
 }
--- AdventOfCode2024Tests/UnitTestDay19.cs
+++ AdventOfCode2024Tests/UnitTestDay19.cs
@@ -1,39 +1,49 @@
 using AdventOfCode2024;
+
 using Common;
 
 namespace Tests
 {
-	[TestClass]
-	public class UnitTestDay19
-	{
-		private Day19 day;
+    [TestClass]
+    public class UnitTestDay19
+    {
+        private Day19 day;
         private string data;
         private string[] testdata;
 
         [TestInitialize]
         public void Init()
         {
-            data = @"";
+            data = @"r, wr, b, g, bwu, rb, gb, br
+
+brwrr
+bggr
+gbbr
+rrbgbr
+ubwu
+bwurrg
+brgr
+bbrgwb";
             testdata = data.SplitOnNewlineArray(false);
 
             day = new Day19(data);
-		}
+        }
 
 
-		[TestMethod("Day 19, Part 1")]
-		[TestCategory("Example data")]
-		public void Part1()
-		{
+        [TestMethod("Day 19, Part 1")]
+        [TestCategory("Example data")]
+        public void Part1()
+        {
+            long result = day.Problem1();
+            Assert.AreEqual(6, result);
+        }
 
-			Assert.AreEqual(1, 1);
-		}
-
-		[TestMethod("Day 19, Part 2")]
-		[TestCategory("Example data")]
-		public void Part2()
-		{
-
-			Assert.AreEqual(2, 2);
-		}
-	}
+        [TestMethod("Day 19, Part 2")]
+        [TestCategory("Example data")]
+        public void Part2()
+        {
+            long result = day.Problem2();
+            Assert.AreEqual(16, result);
+        }
+    }
 }
