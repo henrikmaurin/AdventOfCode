b442f611b4c424b50fef4876fa1952823459cd20
Henrik Maurin <henrik.maurin@soprasteria.com>
2024-12-22 13:36:20 +0100

    Day 22. Shifting

--- AdventOfCode2024/Day22.cs
+++ AdventOfCode2024/Day22.cs
@@ -1,4 +1,6 @@
-﻿using Common;
+﻿using System.Net.Http.Headers;
+
+using Common;
 
 namespace AdventOfCode2024
 {
@@ -11,26 +13,105 @@ namespace AdventOfCode2024
             if (testdata != null)
             {
                 data = testdata.SplitOnNewline();
+                Parse();
                 return;
             }
 
             data = input.GetDataCached().SplitOnNewline();
+            Parse();
         }
+
+        public void Parse()
+        {
+
+        }
+
         public void Run()
         {
-            int result1 = MeasureExecutionTime(() => Problem1());
+            long result1 = MeasureExecutionTime(() => Problem1());
             WriteAnswer(1, "Result: {result}", result1);
 
-            int result2 = MeasureExecutionTime(() => Problem2());
+            long result2 = MeasureExecutionTime(() => Problem2());
             WriteAnswer(2, "Result: {result}", result2);
         }
-        public int Problem1()
+        public long Problem1()
         {
-            return 0;
+            long sum = 0;
+            foreach (string price in data)
+            {
+                sum += GenerateSecretNumber(price.ToLong(), 2000);
+            }
+
+            return sum;
         }
-        public int Problem2()
+        public long Problem2()
+        {          
+            int c1 = 0;
+            int c2 = 0;
+            int c3 = 0;
+            int c4 = 0;
+            int sellernumber = 0;
+          
+            Dictionary<string, long> prices2 = new Dictionary<string, long>();
+
+            foreach (string price in data)
+            {
+                long result = price.ToLong();
+                int lastrmod10 = 0;
+
+                Dictionary<string, int> p = new Dictionary<string, int>();
+
+                for (int i = 0; i < 2000; i++)
+                {
+                    result = GenerateSecretNumber(result, 1);
+                    int rmod10 = (int)result % 10;
+                    c1 = c2;
+                    c2 = c3;
+                    c3 = c4;
+                    c4 = rmod10 - lastrmod10;
+                    lastrmod10 = rmod10;
+
+                    if (i >= 3)
+                    {
+                        string key = $"{c1}:{c2}:{c3}:{c4}";
+                        if (p.ContainsKey(key))
+                        {
+                            continue;
+                        }
+                        p.Add(key, rmod10);
+                        if (prices2.ContainsKey(key))
+                        {
+                            prices2[key] += rmod10;
+                        }
+                        else
+                        {
+                            prices2.Add(key, rmod10);
+                        }
+                    }
+                }
+                sellernumber++;
+            }
+   
+            return prices2.OrderBy(s => s.Value).Select(s => s.Value).Last(); 
+        }
+
+        public long GenerateSecretNumber(long secret, long iterations)
         {
-            return 0;
+            for (int i = 0; i < iterations; i++)
+            {
+                secret = GenerateSecretNumber(secret);
+            }
+
+            return secret;
+        }
+         
+        public long GenerateSecretNumber(long secret)
+        {
+            secret ^= (secret * 64) % 16777216;
+            secret ^= (secret / 32) % 16777216;
+            secret ^= (secret * 2048) % 16777216;            
+           
+            return secret;
         }
     }
 }
--- AdventOfCode2024Tests/UnitTestDay22.cs
+++ AdventOfCode2024Tests/UnitTestDay22.cs
@@ -1,39 +1,59 @@
 using AdventOfCode2024;
+
 using Common;
 
 namespace Tests
 {
-	[TestClass]
-	public class UnitTestDay22
-	{
-		private Day22 day;
+    [TestClass]
+    public class UnitTestDay22
+    {
+        private Day22 day;
         private string data;
         private string[] testdata;
 
         [TestInitialize]
         public void Init()
         {
-            data = @"";
+            data = @"1
+2
+3
+2024";
             testdata = data.SplitOnNewlineArray(false);
 
             day = new Day22(data);
-		}
+        }
 
 
-		[TestMethod("Day 22, Part 1")]
-		[TestCategory("Example data")]
-		public void Part1()
-		{
+        [TestMethod("Day 22, Part 1")]
+        [TestCategory("Example data")]
+        public void Part1()
+        {
+            long result = day.Problem1();
 
-			Assert.AreEqual(1, 1);
-		}
+            long r = day.GenerateSecretNumber(123, 1);
+            Assert.AreEqual(15887950, r);
 
-		[TestMethod("Day 22, Part 2")]
-		[TestCategory("Example data")]
-		public void Part2()
-		{
+            r = day.GenerateSecretNumber(15887950, 1);
+            Assert.AreEqual(16495136, r);
 
-			Assert.AreEqual(2, 2);
-		}
-	}
+            r = day.GenerateSecretNumber(15887950, 1);
+            Assert.AreEqual(16495136, r);
+
+            r = day.GenerateSecretNumber(123, 2);
+            Assert.AreEqual(16495136, r);
+
+            r = day.GenerateSecretNumber(123, 10);
+            Assert.AreEqual(5908254, r);
+
+            Assert.AreEqual(37990510, result);
+        }
+
+        [TestMethod("Day 22, Part 2")]
+        [TestCategory("Example data")]
+        public void Part2()
+        {
+            long result = day.Problem2();
+            Assert.AreEqual(23, result);
+        }
+    }
 }
