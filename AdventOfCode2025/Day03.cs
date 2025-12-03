a86e92166d989d591470f92ad1255da1630b5c5c
MAURIN Henrik <henrik.maurin@soprasteria.com>
2025-12-03 08:50:02 +0100

    Day 3 first

--- AdventOfCode2025/Day03.cs
+++ AdventOfCode2025/Day03.cs
@@ -19,19 +19,83 @@ namespace AdventOfCode2025
         }
         public void Run()
         {
-            int result1 = MeasureExecutionTime(() => Problem1());
+            long result1 = MeasureExecutionTime(() => Problem1());
             WriteAnswer(1, "Result: {result}", result1);
 
-            int result2 = MeasureExecutionTime(() => Problem2());
+            long result2 = MeasureExecutionTime(() => Problem2());
             WriteAnswer(2, "Result: {result}", result2);
         }
-        public int Problem1()
+        public long Problem1()
         {
-            return 0;
+            long sum = 0;
+            foreach (var item in data)
+            {
+                sum += MaxJoltage(item);
+            }
+
+            return sum;
         }
-        public int Problem2()
+        public long Problem2()
         {
-            return 0;
+            long sum = 0;
+            foreach (var item in data)
+            {
+                sum += MaxJoltage(item, 12);
+            }
+
+            return sum;
+        }
+
+        public static int MaxJoltage(string batteryBank)
+        {
+            int max = 0;
+            for (int i = 0; i < batteryBank.Length - 1; i++)
+            {
+                int j1 = batteryBank[i].ToInt() * 10;
+                for (int j = i + 1; j < batteryBank.Length; j++)
+                {
+                    int j2 = batteryBank[j].ToInt() * 1;
+                    if (j1 + j2 > max)
+                        max = j1 + j2;
+                }
+            }
+
+            return max;
+        }
+
+        public static long MaxJoltage(string batteryBank, int targetLength)
+        {
+            List<int> b = new List<int>();
+            foreach (char c in batteryBank)
+            {
+                b.Add(c.ToInt());
+            }
+
+            while (b.Count > targetLength)
+            {
+                bool removed = false;
+                for (int i = 0; i < b.Count -1; i++)
+                {
+                    if (b[i] < b[i + 1])
+                    {
+                        b.RemoveAt(i);
+                        removed = true;
+                        break;
+                    }                   
+                }
+                if (!removed)
+                {
+                    b.RemoveAt(b.Count - 1);
+                }
+            }
+
+            string result = string.Empty;
+            foreach (var c in b)
+            {
+                result += $"{c}";
+            }
+
+            return result.ToLong();
         }
     }
 }
