5bc4febbdd4dbfcbccb70855c5e2a7bf62d841b5
MAURIN Henrik <henrik.maurin@soprasteria.com>
2025-12-04 06:24:44 +0100

    Day 4 ugly

--- AdventOfCode2025/Day04.cs
+++ AdventOfCode2025/Day04.cs
@@ -15,6 +15,16 @@ namespace AdventOfCode2025
             }
 
             data = input.GetDataCached().SplitOnNewline();
+            //            data = @"..@@.@@@@.
+            //@@@.@.@.@@
+            //@@@@@.@.@@
+            //@.@@@@..@.
+            //@@.@@@@.@@
+            //.@@@@@@@.@
+            //.@.@.@.@@@
+            //@.@@@.@@@@
+            //.@@@@@@@@.
+            //@.@.@@@.@.".SplitOnNewline();
         }
         public void Run()
         {
@@ -26,11 +36,110 @@ namespace AdventOfCode2025
         }
         public int Problem1()
         {
-            return 0;
+            int xSize = data[0].Length;
+            int ySize = data.Count;
+
+            Dictionary<(int, int), char> map = new Dictionary<(int, int), char>();
+
+            for (int y = 0; y < data.Count; y++)
+            {
+                for (int x = 0; x < xSize; x++)
+                    map.Add((x, y), data[y][x]);
+            }
+
+            int counter = 0;
+
+            for (int y = 0; y < data.Count; y++)
+            {
+                for (int x = 0; x < xSize; x++)
+                {
+                    int c = 0;
+
+                    if (map[(x, y)] != '@')
+                        continue;
+
+                    char r;
+
+                    if (map.TryGetValue((x - 1, y - 1), out r) && r == '@') c++;
+                    if (map.TryGetValue((x - 1, y), out r) && r == '@') c++;
+                    if (map.TryGetValue((x - 1, y + 1), out r) && r! == '@') c++;
+                    if (map.TryGetValue((x, y - 1), out r) && r == '@') c++;
+                    if (map.TryGetValue((x, y + 1), out r) && r == '@') c++;
+                    if (map.TryGetValue((x + 1, y - 1), out r) && r == '@') c++;
+                    if (map.TryGetValue((x + 1, y), out r) && r == '@') c++;
+                    if (map.TryGetValue((x + 1, y + 1), out r) && r == '@') c++;
+
+
+
+                    if (c < 4)
+                        counter++;
+
+
+                }
+
+            }
+
+
+
+            return counter;
         }
         public int Problem2()
         {
-            return 0;
+            int xSize = data[0].Length;
+            int ySize = data.Count;
+
+            Dictionary<(int, int), char> map = new Dictionary<(int, int), char>();
+
+            for (int y = 0; y < data.Count; y++)
+            {
+                for (int x = 0; x < xSize; x++)
+                    map.Add((x, y), data[y][x]);
+            }
+
+            int counter = 0;
+
+            bool removed = true;
+
+            while (removed)
+            {
+                removed = false;
+                for (int y = 0; y < data.Count; y++)
+                {
+                    for (int x = 0; x < xSize; x++)
+                    {
+                        int c = 0;
+
+                        if (map[(x, y)] != '@')
+                            continue;
+
+                        char r;
+
+                        if (map.TryGetValue((x - 1, y - 1), out r) && r == '@') c++;
+                        if (map.TryGetValue((x - 1, y), out r) && r == '@') c++;
+                        if (map.TryGetValue((x - 1, y + 1), out r) && r! == '@') c++;
+                        if (map.TryGetValue((x, y - 1), out r) && r == '@') c++;
+                        if (map.TryGetValue((x, y + 1), out r) && r == '@') c++;
+                        if (map.TryGetValue((x + 1, y - 1), out r) && r == '@') c++;
+                        if (map.TryGetValue((x + 1, y), out r) && r == '@') c++;
+                        if (map.TryGetValue((x + 1, y + 1), out r) && r == '@') c++;
+
+
+
+                        if (c < 4)
+                        {
+                            removed = true;
+                            map[(x, y)] = '.';
+                            counter++;
+                        }
+
+
+                    }
+
+                }
+
+            }
+
+            return counter;
         }
     }
 }
