26732e03e67fa3015f7e119126d5321852b45a4b
MAURIN Henrik <henrik.maurin@soprasteria.com>
2025-12-11 08:30:14 +0100

    Day 9 geometry

--- AdventOfCode2025/Day09.cs
+++ AdventOfCode2025/Day09.cs
@@ -1,5 +1,7 @@
 ﻿using Common;
 
+using NetTopologySuite.Geometries;
+
 namespace AdventOfCode2025
 {
     public class Day09 : DayBase, IDay
@@ -15,22 +17,99 @@ namespace AdventOfCode2025
             }
 
             data = input.GetDataCached().SplitOnNewline();
+            //            data = @"7,1
+            //11,1
+            //11,7
+            //9,7
+            //9,5
+            //2,5
+            //2,3
+            //7,3".SplitOnNewline();
         }
         public void Run()
         {
-            int result1 = MeasureExecutionTime(() => Problem1());
+            long result1 = MeasureExecutionTime(() => Problem1());
             WriteAnswer(1, "Result: {result}", result1);
 
-            int result2 = MeasureExecutionTime(() => Problem2());
+            long result2 = MeasureExecutionTime(() => Problem2());
             WriteAnswer(2, "Result: {result}", result2);
         }
-        public int Problem1()
+        public long Problem1()
         {
-            return 0;
+            List<(long, long)> points = new List<(long, long)>();
+            foreach (var line in data)
+            {
+                var xy = line.Split(',').ToLong();
+                points.Add((xy[0], xy[1]));
+            }
+
+            long biggest = 0;
+
+            for (int i = 0; i < points.Count - 1; i++)
+            {
+                for (int j = i + 1; j < points.Count; j++)
+                {
+                    long size = (Math.Abs(points[i].Item1 - points[j].Item1) + 1) * (Math.Abs(points[i].Item2 - points[j].Item2) + 1);
+                    if (size > biggest)
+                    {
+                        biggest = size;
+                    }
+
+                }
+            }
+
+            return biggest;
         }
-        public int Problem2()
+        public long Problem2()
         {
-            return 0;
+            List<(long, long)> points = new List<(long, long)>();
+            List<Coordinate> coords = new List<Coordinate>();
+            foreach (var line in data)
+            {
+                var xy = line.Split(',').ToLong();
+                points.Add((xy[0], xy[1]));
+                coords.Add(new Coordinate((int)xy[0], (int)xy[1]));
+            }
+
+            coords.Add(coords[0]);
+
+            var geometryFactory = new GeometryFactory();
+            var polygon = geometryFactory.CreatePolygon(coords.ToArray());
+
+            long biggest = 0;
+
+            for (int i = 0; i < points.Count - 1; i++)
+            {
+                Coordinate coord = points[i];
+
+                for (int j = i + 1; j < points.Count; j++)
+                {
+                    Coordinate coord2 = points[j];
+                    var env = new Envelope(coord, coord2);
+                    var rectangle = geometryFactory.ToGeometry(env);
+                    if (rectangle.Within(polygon)){
+                        long size = (Math.Abs(points[i].Item1 - points[j].Item1) + 1) * (Math.Abs(points[i].Item2 - points[j].Item2) + 1);
+                        if (size > biggest)
+                        {
+                            biggest = size;
+                        }
+                    }
+
+                }
+            }
+
+            return biggest;
         }
+
+        bool IsInside(Coordinate[] inner, Coordinate[] outer)
+        {
+            var gf = new GeometryFactory();
+
+            var innerPoly = gf.CreatePolygon(inner);
+            var outerPoly = gf.CreatePolygon(outer);
+
+            return outerPoly.Contains(innerPoly);
+        }
+
     }
 }
--- AdventOfCode2025Tests/UnitTestDay09.cs
+++ AdventOfCode2025Tests/UnitTestDay09.cs
@@ -7,16 +7,23 @@ namespace Tests
 	public class UnitTestDay09
 	{
 		private Day09 day;
-        private string data;
-        private string[] testdata;
+		private string data;
+		private string[] testdata;
 
-        [TestInitialize]
-        public void Init()
-        {
-            data = @"";
-            testdata = data.SplitOnNewlineArray(false);
+		[TestInitialize]
+		public void Init()
+		{
+			data = @"7,1
+11,1
+11,7
+9,7
+9,5
+2,5
+2,3
+7,3";
+			testdata = data.SplitOnNewlineArray(false);
 
-            day = new Day09(data);
+			day = new Day09(data);
 		}
 
 
@@ -24,16 +31,16 @@ namespace Tests
 		[TestCategory("Example data")]
 		public void Part1()
 		{
-
-			Assert.AreEqual(1, 1);
+			long result = day.Problem1();
+			Assert.AreEqual(50, result);
 		}
 
 		[TestMethod("Day 9, Part 2")]
 		[TestCategory("Example data")]
 		public void Part2()
 		{
-
-			Assert.AreEqual(2, 2);
+			long result = day.Problem2();
+			Assert.AreEqual(24, result);
 		}
 	}
 }
