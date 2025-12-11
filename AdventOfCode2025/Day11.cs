7f627b520b193e8075f27210833eadab41c9314d
MAURIN Henrik <henrik.maurin@soprasteria.com>
2025-12-11 08:31:34 +0100

    Day 11

--- AdventOfCode2025/Day11.cs
+++ AdventOfCode2025/Day11.cs
@@ -6,31 +6,94 @@ namespace AdventOfCode2025
     {
         private const int day = 11;
         List<string> data;
+        Dictionary<string, string[]> devices = new Dictionary<string, string[]>();
+
+
         public Day11(string? testdata = null) : base(Global.Year, day, testdata != null)
         {
             if (testdata != null)
             {
                 data = testdata.SplitOnNewline();
+                foreach (string s in data)
+                {
+                    var label = s.Split(':').First();
+                    var connections = s.Split(":").Last().Trim().Split(" ");
+                    devices.Add(label, connections);
+                }
                 return;
             }
 
-            data = input.GetDataCached().SplitOnNewline();
+            data = input.GetDataCached().SplitOnNewline();         
+            foreach (string s in data)
+            {
+                var label = s.Split(':').First();
+                var connections = s.Split(":").Last().Trim().Split(" ");
+                devices.Add(label, connections);
+            }
+
+
         }
         public void Run()
         {
-            int result1 = MeasureExecutionTime(() => Problem1());
+            long result1 = MeasureExecutionTime(() => Problem1());
             WriteAnswer(1, "Result: {result}", result1);
 
-            int result2 = MeasureExecutionTime(() => Problem2());
+            long result2 = MeasureExecutionTime(() => Problem2());
             WriteAnswer(2, "Result: {result}", result2);
         }
-        public int Problem1()
+        public long Problem1()
         {
-            return 0;
+            long result = 0;
+
+            result = Traverse("you",true,true);
+
+            return result;
         }
-        public int Problem2()
+        public long Problem2()
         {
-            return 0;
+            long result = 0;
+
+            result = Traverse("svr",false,false);
+
+            return result;
+        }       
+
+        long Traverse(string device, bool hasdac, bool hasfft)
+        {
+            string key = $"{device},{hasdac},{hasfft}";
+
+            if (Memoize.TryGet(nameof(Traverse), key, out long memo))
+            {
+                return memo;
+            }
+
+            if (device == "dac")
+            {
+                hasdac = true;  
+            }
+
+            if ( device=="fft")
+            {
+                  hasfft = true;  
+            }
+
+            if (device == "out")
+            {
+                if (hasdac && hasfft)
+                    return 1;
+                return 0;
+            }
+
+            long result = 0;
+
+            foreach (var connection in devices[device])
+            {
+                result += Traverse(connection,hasdac,hasfft);
+            }
+
+            Memoize.Set(nameof(Traverse), key, result);
+
+            return result;
         }
     }
 }
--- AdventOfCode2025Tests/UnitTestDay11.cs
+++ AdventOfCode2025Tests/UnitTestDay11.cs
@@ -1,39 +1,64 @@
 using AdventOfCode2025;
+
 using Common;
 
 namespace Tests
 {
-	[TestClass]
-	public class UnitTestDay11
-	{
-		private Day11 day;
+    [TestClass]
+    public class UnitTestDay11
+    {
+        private Day11 day;
         private string data;
         private string[] testdata;
 
         [TestInitialize]
         public void Init()
         {
-            data = @"";
-            testdata = data.SplitOnNewlineArray(false);
 
+        }
+
+
+        [TestMethod("Day 11, Part 1")]
+        [TestCategory("Example data")]
+        public void Part1()
+        {
+            data = @"aaa: you hhh
+you: bbb ccc
+bbb: ddd eee
+ccc: ddd eee fff
+ddd: ggg
+eee: out
+fff: out
+ggg: out
+hhh: ccc fff iii
+iii: out"; 
             day = new Day11(data);
-		}
+           long result = day.Problem1();
+            Assert.AreEqual(5, result);
+        }
 
+        [TestMethod("Day 11, Part 2")]
+        [TestCategory("Example data")]
+        public void Part2()
+        {
+            data = @"svr: aaa bbb
+aaa: fft
+fft: ccc
+bbb: tty
+tty: ccc
+ccc: ddd eee
+ddd: hub
+hub: fff
+eee: dac
+dac: fff
+fff: ggg hhh
+ggg: out
+hhh: out"; 
+            day = new Day11(data);
+ 
+            long result = day.Problem2();
 
-		[TestMethod("Day 11, Part 1")]
-		[TestCategory("Example data")]
-		public void Part1()
-		{
-
-			Assert.AreEqual(1, 1);
-		}
-
-		[TestMethod("Day 11, Part 2")]
-		[TestCategory("Example data")]
-		public void Part2()
-		{
-
-			Assert.AreEqual(2, 2);
-		}
-	}
+            Assert.AreEqual(2, result);
+        }
+    }
 }
